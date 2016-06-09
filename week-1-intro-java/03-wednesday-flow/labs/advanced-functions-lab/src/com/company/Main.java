c56b62570ef847cb5ec4c1c8961368c5ac52bbb1
Vighnesh Ajay Rege <NehaRege@Nehas-MacBook-Pro.local>
2016-06-08 21:02:56 -0700

    Adding 2 labs: collections-lab and advanced-functions-lab  -Neha

--- week-1-intro-java/03-wednesday-flow/labs/advanced-functions-lab/src/com/company/Main.java
+++ week-1-intro-java/03-wednesday-flow/labs/advanced-functions-lab/src/com/company/Main.java
@@ -1,4 +1,5 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
@@ -11,15 +12,24 @@ public class Main {
 
         printLetters("Heqwlooewlo");
 
-        isGreaterThan50();
+        double rand = isGreaterThan50();
 
-        date(20,02,1992);
+        date(20, 02, 1992);
 
-        newWord("hi");
+//        newWord("hi");
 
+        printNosExcept4(9);
 
+        System.out.println("The sum is: "+checkEqualityAndAdd(5,4));
+
+        System.out.println("The sum is: "+checkEqualityAndAdd(2,2));
+
+        countryDomain();
+
+        vowelConsonantDetector();
     }
 
+    //function 1
     public static boolean oddEven(int num) {
         System.out.println("Function 1:");
         if (num % 2 == 0) {
@@ -31,6 +41,7 @@ public class Main {
         }
     }
 
+    //function 2
     public static boolean wordComparator(String w1, String w2) {
         System.out.println("Function 2:");
         String temp1 = w1.toLowerCase();
@@ -44,6 +55,7 @@ public class Main {
         }
     }
 
+    //function 3
     public static void printLetters(String w1) {
         System.out.println("Function 3:");
         int i = 0;
@@ -53,53 +65,121 @@ public class Main {
         }
     }
 
-    public static void isGreaterThan50(){
+    //function 4
+    public static double isGreaterThan50() {
         System.out.println("Function 4:");
-        double num= Math.random();
-        System.out.println("The random number generated is "+num);
-        if(num > 50){
-            System.out.println("The number generated is "+num);
-        }
-        else {
-            System.out.println("So the number is less than 50. So returning: -1\n");
+        double num = Math.random();
+        System.out.println("The random number generated is " + num);
+        if (num > 50) {
+            System.out.println("The number generated is " + num);
+            return num;
+        } else {
+            System.out.println("So the number is less than 50. \nSo returning: -1\n");
+            return -1;
         }
     }
 
-    public static void date(int day, int month, int year){
+    //function 5
+    public static void date(int day, int month, int year) {
         System.out.println("Function 5:");
-        int today_date=day+1;
-//        int today_date=day++;
-
-        System.out.println("day: "+today_date+" month: "+month+" year: "+year);
+        int today_date = day + 1;
+        System.out.println("day:" + today_date + "  month:" + month + "  year:" + year);
     }
 
-    public static void newWord(String w1){
-        System.out.println("Function 6:");
-        String repl = w1.replaceAll("hi","hello");
+    //function 6
+//    public static void newWord(String w1) {
+//        System.out.println("Function 6:");
+//        int i = 0;
+//        do {
+//
+//        }
+//
+//        System.out.println(repl);
+//
+//    }
 
-        System.out.println(repl);
+//    public static void number_printing(int num) {
+//        if (num <= 5) {
+//            System.out.println("Need a number greater than 5");
+//        } else {
+//            int no = 1;
+//            while (num)
+//        }
+//    }
 
+    //function 7
+    public static void printNosExcept4(int num) {
+        if (num > 5) {
+            int i = 1;
+            while (i < 4) {
+                System.out.println(i);
+                i++;
+            }
+            int j = 5;
+            while (j<=num) {
+                System.out.println(j);
+                j++;
+            }
+        }
+        else System.out.println("No should be greater than 5");
+        System.out.println("");
     }
 
-    public static void number_printing(int num){
-        if(num <=5){
-            System.out.println("Need a number greater than 5");
+    //function 8
+    public static int checkEqualityAndAdd(int a,int b){
+        int sum;
+        if(a==b){
+            a=a+2;
+            b=b+2;
+            sum=a+b;
+            return sum;
         }
         else {
-            int no=1;
-            while (num )
+            a++;
+            b++;
+            sum=a+b;
+            return sum;
         }
     }
 
+    //function 9
+    public static void countryDomain(){
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter a country domain: ");
+        String domain = reader.nextLine();
 
+        switch(domain){
+            case "us":
+            case "US":
+                System.out.println("United States");
+                break;
+            case "de":
+            case "DE":
+                System.out.println("Germany");
+                break;
+            case "hu":
+            case "HU":
+                System.out.println("Hungary");
+                break;
+            default:
+                System.out.println("Unknown");
+                break;
+        }
+    }
 
-
-
-
-
-
-
+    //function 10
+    public static void vowelConsonantDetector(){
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter a letter: ");
+        char letter=reader.next().charAt(0);
+        if(letter == 'a' || letter =='e' || letter =='i' || letter=='o' || letter=='u'){
+            System.out.println("The letter is a vowel");
+        }
+        else System.out.println("The letter is a consonant");
+    }
 }
+
+
 
 
 
--- week-1-intro-java/03-wednesday-flow/labs/collections-lab/src/com/company/Main.java
+++ week-1-intro-java/03-wednesday-flow/labs/collections-lab/src/com/company/Main.java
@@ -1,23 +1,47 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Main {
-    boolean palind;
 
     public static void main(String[] args) {
         // write your code here
-        palindrome("dagd");
+        palindrome("Dadss");
 
+        int[] a={1,2,43,65,0,100};
+        System.out.println("Max is: "+maxNo(a));
 
+        int days=noOfDaysInTheMonth("January");
+
+        int[] numAddSub={2,2,2,10,30};
+        int result =addSub(numAddSub);
+        System.out.println("The result of numAddSub function is: "+result);
+
+        omitChars();
+
+        omitVowels("heyyyyhiii");
+
+        String[] cList={"US","India","UK","France","China"};
+        swapCountry(cList);
+
+        ArrayList<String> names =new ArrayList<>();
+        names.add("neha");
+        names.add("hello");
+        names.add("hello");
+        uniqueList(names);
     }
 
+//Function 1:
+
     public static void palindrome(String originalStr){
-        String reversedStr=reverseString(originalStr);
-        String lowCaseReversedStr=reversedStr.toLowerCase();
-        if(lowCaseReversedStr.equals(originalStr)){
-            System.out.println("The string "+originalStr+" ")
+        String lowCaseOriginalStr=originalStr.toLowerCase();
+        String reversedStr=reverseString(lowCaseOriginalStr);
+        if(reversedStr.equals(lowCaseOriginalStr)){
+            System.out.println("The string "+originalStr+" is a palindrome");
         }
-
-
+        else{
+            System.out.println("The string "+originalStr+" is not a palindrome");
+        }
     }
 
     public static String reverseString(String input){
@@ -27,31 +51,172 @@ public class Main {
         }
         return temp;
     }
+
+    //Function 2:  (Incomplete......)
+
+    public static void omitChars(){
+        ArrayList<Character> list= new ArrayList<>();
+        list.add('a');
+        list.add('b');
+        list.add('c');
+        list.add('d');
+
+        int i=0;
+        while (i%2==0 && i<list.size()){
+            list.remove(i);
+
+            i++;
+        }
+//        System.out.println(list);
+//        String str=list.toString();
+        System.out.println(list);
+
+    }
+
+//    public static void omitChars(){
+//        char[] list={'a','b','c','d'};
+//        int i=0;
+//        while (i%2==0 && i<list.length){
+//            list[i]=list[i+1];
+//            i++;
+//        }
+//        for(int j=0;j<list.length;j++){
+//            System.out.println(list[j]);
+//        }
+////        System.out.println("The new list is: "+list);
+////        for(int i=0;i<list.length;i++){
+////        }
+//    }
+
+    //Function 3:
+
+    public static int maxNo(int[] a){
+        int max=a[0];
+        for(int i=0;i<a.length;i++){
+            if(a[i]>max){
+                max=a[i];
+            }
+        }
+        return max;
+    }
+
+    //Function 4:
+
+    public static int noOfDaysInTheMonth(String month){
+
+            switch(month){
+                case "January":System.out.println("31 days");
+                    return 31;
+//                    break;
+                case "March":System.out.println("31 days");
+                    return 31;
+//                    break;
+                case "May":System.out.println("31 days");
+                    return 31;
+//                    break;
+                case "July":System.out.println("31 days");
+                    return 31;
+//                    break;
+                case "August":System.out.println("31 days");
+                    return 31;
+//                    break;
+                case "October":System.out.println("31 days");
+                    return 31;
+//                    break;
+                case "December": System.out.println("31 days");
+                    return 31;
+//                    break;
+                case "February":System.out.println("28 days");
+                    return 28;
+//                    break;
+                case "April":System.out.println("30 days");
+                    return 30;
+//                    break;
+                case "June":System.out.println("30 days");
+                    return 30;
+//                    break;
+                case "September":System.out.println("30 days");
+                    return 30;
+//                    break;
+                case "November":System.out.println("30 days");
+                    return 30;
+//                    break;
+                default:
+                    System.out.println("Not a valid month");
+                    return 0;
+//                    break;
+            }
+    }
+
+    //Function 5:
+
+    public static int addSub(int[] num){
+        int newSum=0;
+        for(int i=0;i<num.length;i++){
+            if(num[i]>1 && num[i]<20){
+                newSum=newSum+num[i];
+            }
+            if (num[i]>20){
+                newSum=newSum-3;
+            }
+        }
+        return newSum;
+    }
+
+    //funtion 6:   (Incomplete......)
+
+    public static void uniqueList(ArrayList<String> names) {
+
+        String first ="";
+        ArrayList<String> newSt = new ArrayList<>();
+        for (int j = 0; j < names.size(); j++) {
+
+            for (int i = 1; i < names.size(); i++) {
+//            String first=names.get(i);
+                first = names.get(j);
+                if (first.equals(names.get(i))) {
+                    names.remove(i);
+                }
+            }
+        }
+            System.out.println("Unique list is:");
+            for (int i = 0; i <names.size(); i++) {
+                System.out.println(names.get(i));
+            }
+    }
+
+    //function 7:
+
+    public static void omitVowels(String s) {
+        if (s.length() == 0) {
+            System.out.println("String empty!");
+        } else {
+            String newS = s.replaceAll("[aAeEiIoOuU]", "");
+            System.out.println(newS);
+        }
+    }
+
+    //function 8:
+
+    public static void swapCountry(String[] cList){
+        String temp1="";
+        String temp2="";
+        temp1=cList[0];
+        temp2=cList[cList.length-1];
+
+        //swapping
+        cList[0]=temp2;
+        cList[cList.length-1]=temp1;
+
+        //printing
+        System.out.println("The swapped list is:");
+        for(int i=0;i<cList.length;i++){
+            System.out.println(cList[i]);
+        }
+    }
 }
 
 
-//    //Function1
-//    public static void palindrome(String s) {
-//        String newS = s.toLowerCase();
-//
-//        for (int i = 0; i < s.length(); i++) {
-//
-//            if (s.charAt(i) == s.charAt(s.length() - 1) && s.charAt()) {
-//                System.out.println("The string " + s + " is a palindrome");
-////                return true;
-//            } else{
-//                System.out.println("The string " + s + " is not a palindrome");
-////                return false;
-//            }
-//
-////            if (s.charAt(i) == s.charAt(s.length() - 1 - i)) {
-////                System.out.println("The string " + s + " is a palindrome");
-//////                return true;
-////            } else{
-////                System.out.println("The string " + s + " is not a palindrome");
-//////                return false;
-////            }
-//        }
-//    }
+
 
 
