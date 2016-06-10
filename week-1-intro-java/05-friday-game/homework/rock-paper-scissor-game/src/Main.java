bf8eb93c8b984e015c8d4434dc31e9e61b8f9454
“[firstname <[valid-email]>
2016-06-10 09:33:40 -0700

    Fix File Writer

--- week-1-intro-java/05-friday-game/homework/rock-paper-scissor-game/src/Main.java
+++ week-1-intro-java/05-friday-game/homework/rock-paper-scissor-game/src/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
         public static void main(String... args) throws IOException {
 
-            //String fileName = "test.txt";
+            String fileName = "test.txt";
 
             int playgame;
             int number_Random = 0;
@@ -30,10 +30,10 @@ public class Main {
            // outputStream.close();
            // System.out.println();
 
-            //FileWriter fw = new FileWriter(fileName);
-           // BufferedWriter bw = new BufferedWriter(fw);
-            //bw.write("Computer as Rock");
-            //bw.close();
+            FileWriter fw = new FileWriter(fileName);
+            BufferedWriter bw = new BufferedWriter(fw);
+
+
             //PrintWriter pw = new PrintWriter(fw);
             //pw.println("Computer as Rock");
             //pw.close();
@@ -46,46 +46,63 @@ public class Main {
                 case 0:
                     int number_Random1 = 1;
                     System.out.println("Computer as Rock");
+                    bw.write("Computer as Rock");
+
                     if (playgame == (number_Random1)) {
                         System.out.println("same !!!");
-
+                        bw.write("same !!!");
                     } else if (playgame == (2)){
                         System.out.println("you win !!! ");
+                        bw.write("you win !!!");
 
                     } else if (playgame == (3)) {
                         System.out.println("you Lost !!!    ");
+                        bw.write("you Lost !!!");
                     } else {
                         System.out.println("Please input a number !");
-                    }
+                    }bw.close();
                     break;
 
                 case 1:
                     int number_Random2 = 2;
                     System.out.println("Computer as Paper");
+                    bw.write("Computer as Paper");
+
                     if (playgame == (number_Random2)) {
                         System.out.println("same !!!");
+                        bw.write("same !!!");
                     } else if (playgame == (3)){
                         System.out.println("you win !!! ");
+                        bw.write("you win !!! ");
                     } else if (playgame == (1)) {
                         System.out.println("you Lost !!!    ");
+                        bw.write("you Lost !!! ");
                     } else {
                         System.out.println("Please play the game !");
-                    }
+                    }bw.close();
                     break;
 
                 case 2:
                     int number_Random3 = 3;
                     System.out.println("Computer as Scissors");
+                    bw.write("Computer as Scissors");
+
                     if (playgame == (number_Random3)) {
                         System.out.println("same !!!");
+                        bw.write("You Have the same");
+
                     } else if (playgame == (1)){
                         System.out.println("you win !!! ");
+                        bw.write("You win");
+
                     } else if (playgame == (2)) {
                         System.out.println("you Lost !!!    ");
+                        bw.write("You Lost");
+
                     } else {
                         System.out.println("Please play the game !");
 
-                    }
+                    }bw.close();
                     break;
 
 
