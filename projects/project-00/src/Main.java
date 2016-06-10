69ef72f457e04aa94670a557170e5a2119039047
Patrick Cummins <patrickcummins2009@gmail.com>
2016-06-10 14:22:13 -0700

    Added Hash to Rock Paper Scissors, doesn't work yet

--- projects/project-00/src/Main.java
+++ projects/project-00/src/Main.java
@@ -1,15 +1,33 @@
 import java.io.*;
+import java.util.HashMap;
 import java.util.Scanner;
 
 /**
  * Created by patrickcummins on 6/9/16.
  */
-public class Main {
+public class    Main {
+    public static HashMap<Integer, String> historyHash = new HashMap<>();
+    public static int hashMapCurrentKey = 0;
+    public static int textFileCurrent=0;
 
     static public void main (String [] args) throws IOException {
 
+        populateHash();
         menu();
+
+
     }
+
+    public static void populateHash() throws IOException {
+        Scanner myScanner = new Scanner(new File("history.txt"));
+        while(myScanner.hasNextLine()) {
+            historyHash.put(hashMapCurrentKey, myScanner.nextLine());
+            hashMapCurrentKey++;
+            textFileCurrent++;
+        }
+
+    }
+
     public static void menu() throws IOException {
         Scanner myScanner = new Scanner(System.in);
         String playOrHistory;
@@ -24,7 +42,7 @@ public class Main {
             printHistory();
             menu();
         } else if (playOrHistory.equals("3") || playOrHistory.equals("quit")) {
-            System.exit(1);
+            writeHashToText();
         } else{
             System.out.println("Invalid Input");
             menu();
@@ -36,76 +54,86 @@ public class Main {
 
     }
 
+    public static void writeHashToText() throws IOException {
+        FileWriter myWriter = new FileWriter("history.txt", true);
+        for (int i = textFileCurrent + 1; i < historyHash.size(); i++) {
+            myWriter.append(historyHash.get(i));
+
+        }
+        myWriter.close();
+    }
+
     private static void printHistory() throws FileNotFoundException {
-        Scanner myScanner = new Scanner(new File("history.txt"));
+
         int wins = 0;
         int losses = 0;
         int ties = 0;
+        int currentHashKey=0;
         String current;
-        while(myScanner.hasNextLine()){
-            current = myScanner.nextLine();
-            System.out.println(current);
-            if(current.equals("Tie ")){
-                ties+=1;
 
-            } else if(current.equals("Win ")){
-                wins++;
-            }else if(current.equals("Loss ")){
-                losses++;
+            while (currentHashKey< historyHash.size()) {
+                current = historyHash.get(currentHashKey);
+                System.out.println(current);
+                if (current.equals("Tie")) {
+                    ties += 1;
+
+                } else if (current.equals("Win")) {
+                    wins++;
+                } else if (current.equals("Loss")) {
+                    losses++;
+                }
+                currentHashKey++;
             }
 
-
-        }
         System.out.println("Total Wins: " +wins+"\nTotal Losses: "+ losses+"\nTotal Ties: " + ties);
     }
 
     public static void playGame() throws IOException {
         String choice;
         Scanner myScanner = new Scanner(System.in);
-        FileWriter myWriter = new FileWriter("history.txt", true);
-
         System.out.println("Choose: \n1(Rock) \n2(Paper) \n3(Scissors)");
         choice = myScanner.nextLine().toLowerCase();
         int computerChoice = (int) Math.floor(Math.random() * 3) + 1;
         if (choice.equals("rock") || choice.equals("1")){
             if (computerChoice ==1){
                 System.out.println("Computer chooses rock, you tie");
-                myWriter.append("Tie \n");
+                historyHash.put(hashMapCurrentKey, "Tie");
+
             } else if(computerChoice==2){
                 System.out.println("Computer chooses paper, you lose :(");
-                myWriter.append("Loss \n");
+                historyHash.put(hashMapCurrentKey, "Loss");
             }else if(computerChoice ==3){
                 System.out.println("Computer chooses scissors, you Win!");
-                myWriter.append("Win \n");
+                historyHash.put(hashMapCurrentKey, "Win");
             }
 
         }else if(choice.equals("paper") || choice.equals("2")){
             if (computerChoice ==1){
                 System.out.println("Computer chooses rock, you Win!");
-                myWriter.append("Win \n");
+                historyHash.put(hashMapCurrentKey, "Win");
             } else if(computerChoice==2){
                 System.out.println("Computer chooses paper, you tie");
-                myWriter.append("Tie \n");
+                historyHash.put(hashMapCurrentKey, "Tie");
             }else if(computerChoice ==3){
                 System.out.println("Computer chooses scissors, you lose :(");
-                myWriter.append("Loss \n");
+                historyHash.put(hashMapCurrentKey, "Loss");
             }
         }else if (choice.equals("scissors") || choice.equals("3")){
             if (computerChoice ==1){
                 System.out.println("Computer chooses rock, you lose :(");
-                myWriter.append("Loss \n");
+                historyHash.put(hashMapCurrentKey, "Loss");
             } else if(computerChoice==2){
                 System.out.println("Computer chooses paper, you Win!");
-                myWriter.append("Win \n");
+                historyHash.put(hashMapCurrentKey, "Win");
             }else if(computerChoice ==3){
                 System.out.println("Computer chooses scissors, you tie");
-                myWriter.append("Tie \n");
+                historyHash.put(hashMapCurrentKey, "Tie");
             }
         }else{
             System.out.println("Sorry that input isn't accepted");
             playGame();
         }
-        myWriter.close();
+        hashMapCurrentKey++;
         playAgain();
 
     }
