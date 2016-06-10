229b3660572fc1fbc3dda140f28a3b47889bedf6
“[firstname <[valid-email]>
2016-06-10 15:45:54 -0700

    new fix

--- week-1-intro-java/05-friday-game/homework/rock-paper-scissor-game/src/Main.java
+++ week-1-intro-java/05-friday-game/homework/rock-paper-scissor-game/src/Main.java
@@ -5,110 +5,164 @@ import java.io.*;
 import java.util.Random;
 import java.util.Scanner;
 
+import static java.util.Scanner.*;
+
 
 public class Main {
 
 
         public static void main(String... args) throws IOException {
 
+            //boolean run = true;
+
             String fileName = "test.txt";
+
 
             int playgame;
             int number_Random = 0;
+
+
+            Scanner sc = new Scanner(System.in);
+                        System.out.println("=========================================================================");
+                        System.out.println("                                Welcome                                  ");
+                        System.out.println("=========================================================================");
+                        System.out.println("=========================================================================");//Scanner Util
+                        System.out.println("=========================================================================");
+                        System.out.println("                         Do You Want to Play ?"                           );
+                        System.out.println("=========================================================================");
+                        System.out.println("                         For Rock input 1.....                           ");
+                        System.out.println("                         For Paper input 2....                           ");
+                        System.out.println("                         For Scissors input 3.                           ");
+                        System.out.println("=========================================================================");    //Imput int next line
+            playgame = sc.nextInt();
+
+
             Random rd = new Random();                           // new Random Util
             number_Random = rd.nextInt(3);                       //Random Namber will stop at 3
-
-            Scanner sc = new Scanner(System.in);                    //Scanner Util
-
-            System.out.println("enter your choices......");
-            System.out.println("For Rock input 1......");
-            System.out.println("For Paper input 2......");
-            System.out.println("For Scissors input 3...");         //Imput int next line
-
-            //PrintWriter outputStream = new PrintWriter(fileName);  //
-           // outputStream.println("Computer as Rock");
-           // outputStream.close();
-           // System.out.println();
 
             FileWriter fw = new FileWriter(fileName);
             BufferedWriter bw = new BufferedWriter(fw);
 
 
-            //PrintWriter pw = new PrintWriter(fw);
-            //pw.println("Computer as Rock");
-            //pw.close();
 
 
-            playgame = sc.nextInt();
+            if (number_Random == 0)     {System.out.println("                         Computer picked Rock                        ");
+            }
+            else if (number_Random == 1){System.out.println("                         Computer picked Paper                       ");
+            }else                       {System.out.println("                         Computer picked Scissors                    ");
+            }
+            System.out.println("=========================================================================");
 
+
+
+
+
+            if (playgame == 1)          {System.out.println("                           Player picked Rock                        ");
+            }
+            else if (playgame == 2)     {System.out.println("                         Player picked Paper                         ");
+            }else if (playgame == 3)    {System.out.println("                       Player picked Scissors                        ");
+            }else                       {System.out.println("                    WRONG INPUT PLEASE PLAY AGAIN"                    );}
 
             switch (number_Random) {
                 case 0:
                     int number_Random1 = 1;
-                    System.out.println("Computer as Rock");
-                    bw.write("Computer as Rock.......");
+                        System.out.println("=========================================================================");
+                        bw.write(" Computer as Rock  ");
+                        System.out.println("=========================================================================");
+
 
                     if (playgame == (number_Random1)) {
-                        System.out.println("same !!!");
-                        bw.write("same !!!");
+                        System.out.println("=========================================================================");
+                        System.out.println("                          you have the same !!!                          ");
+                        bw.write(" you have the same !!!");
+                        System.out.println("=========================================================================");
+
                     } else if (playgame == (2)){
-                        System.out.println("you win !!! ");
-                        bw.write("you win !!!");
+                        System.out.println("=========================================================================");
+                        System.out.println("                              you win !!!                                ");
+                        bw.write("    you win !!!     ");
+                        System.out.println("=========================================================================");
 
                     } else if (playgame == (3)) {
-                        System.out.println("you Lost !!!    ");
-                        bw.write("you Lost !!!");
+                        System.out.println("=========================================================================");
+                        System.out.println("                             you Lost !!!                                ");
+                        bw.write("    you Lost !!!    ");
+                        System.out.println("=========================================================================");
+
                     } else {
                         System.out.println("Please input a number !");
-                    }bw.close();
+                    }
                     break;
 
                 case 1:
                     int number_Random2 = 2;
-                    System.out.println("Computer as Paper");
-                    bw.write("Computer as Paper.......");
+                        System.out.println("=========================================================================");
+                        bw.write(" Computer as Paper !!!  ");
+                        System.out.println("=========================================================================");
 
                     if (playgame == (number_Random2)) {
-                        System.out.println("same !!!");
-                        bw.write("same !!!");
+                        System.out.println("=========================================================================");
+                        System.out.println("                         you have the same !!!");
+                        bw.write(" you have the same !!!");
+                        System.out.println("=========================================================================");
+
                     } else if (playgame == (3)){
-                        System.out.println("you win !!! ");
+                        System.out.println("=========================================================================");
+                        System.out.println("                              you win !!!                                ");
                         bw.write("you win !!! ");
+                        System.out.println("=========================================================================");
+
                     } else if (playgame == (1)) {
-                        System.out.println("you Lost !!!    ");
+                        System.out.println("=========================================================================");
+                        System.out.println("                              you Lost !!!                               ");
                         bw.write("you Lost !!! ");
+                        System.out.println("=========================================================================");
+
                     } else {
-                        System.out.println("Please play the game !");
-                    }bw.close();
+                        System.out.println("                       Please play the game !"                            );
+                    }
                     break;
 
                 case 2:
                     int number_Random3 = 3;
-                    System.out.println("Computer as Scissors");
-                    bw.write("Computer as Scissors.......");
+                        System.out.println("=========================================================================");
+                        bw.write("  Computer as Scissors !!! ");
+                        System.out.println("=========================================================================");
 
                     if (playgame == (number_Random3)) {
-                        System.out.println("same !!!");
+                        System.out.println("=========================================================================");
+                        System.out.println("                           you have the same !!!");
                         bw.write("You Have the same");
+                        System.out.println("=========================================================================");
 
                     } else if (playgame == (1)){
-                        System.out.println("you win !!! ");
+                        System.out.println("=========================================================================");
+                        System.out.println("                               you win !!!                               ");
                         bw.write("You win");
+                        System.out.println("=========================================================================");
 
                     } else if (playgame == (2)) {
-                        System.out.println("you Lost !!!    ");
+                        System.out.println("=========================================================================");
+                        System.out.println("                              you Lost !!!                               ");
                         bw.write("You Lost");
+                        System.out.println("=========================================================================");
 
                     } else {
-                        System.out.println("Please play the game !");
+                        System.out.println("                         Please play the game !                           ");
+                    }
 
-                    }bw.close();
                     break;
-
-
             }
+            sc.close();
+            bw.close();
+
+
         }
+
+
     }
+
+
 
 
 
