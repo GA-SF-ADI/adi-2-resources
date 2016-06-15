00734d5b7f2d510dcf8f14a2fc77c40d8062e4f1
GA Guest <gaguest@Generals-MacBook-Air.local>
2016-06-15 10:36:28 -0700

    saving current work on Wednesday

--- week-2-intro-android/08-wednesday-activities/lessons/debugging-in-android-1/starter-code/app/src/main/java/co/ga/toasts/ToastActivity.java
+++ week-2-intro-android/08-wednesday-activities/lessons/debugging-in-android-1/starter-code/app/src/main/java/co/ga/toasts/ToastActivity.java
@@ -3,13 +3,28 @@ package co.ga.toasts;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class ToastActivity extends AppCompatActivity {
+
+    Button button = (Button) findViewById(R.id.buyButton);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_toast);
+
+        View.OnClickListener buyOnClickListener = new View.OnClickListener(){
+
+            public void onClick(View v){
+
+             Toast.makeText(ToastActivity.this, "Toast.", Toast.LENGTH_LONG)
+                     .show();
+            }
+
+
+
+        };
     }
 }
