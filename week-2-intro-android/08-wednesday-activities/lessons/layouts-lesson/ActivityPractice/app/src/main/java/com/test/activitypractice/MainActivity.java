9bd8a0d5eceb31e199e364b317f21fd896352afc
Vighnesh Ajay Rege <NehaRege@Nehas-MacBook-Pro.local>
2016-06-15 16:40:34 -0700

    saving changes

--- week-2-intro-android/08-wednesday-activities/lessons/layouts-lesson/ActivityPractice/app/src/main/java/com/test/activitypractice/MainActivity.java
+++ week-2-intro-android/08-wednesday-activities/lessons/layouts-lesson/ActivityPractice/app/src/main/java/com/test/activitypractice/MainActivity.java
@@ -1,6 +1,5 @@
-package com.example.jeanweatherwax.scratchpad;
+package com.test.activitypractice;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,14 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int birth=5;
-        Intent thirdIntent = new Intent(MainActivity.this,ThirdActivity.class);
-        thirdIntent.putExtra("Birthday on: ",birth);
-
-        startActivity(thirdIntent);
-
-
-
-
     }
 }
