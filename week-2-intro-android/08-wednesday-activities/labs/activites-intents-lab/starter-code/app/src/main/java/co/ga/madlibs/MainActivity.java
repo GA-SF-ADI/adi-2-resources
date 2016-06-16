84d40888309b9d75c27421c901e94fb7cbef2387
langsmith <langstonlmcs@gmail.com>
2016-06-15 18:04:11 -0700

    added submit button onclicklistener

--- week-2-intro-android/08-wednesday-activities/labs/activites-intents-lab/starter-code/app/src/main/java/co/ga/madlibs/MainActivity.java
+++ week-2-intro-android/08-wednesday-activities/labs/activites-intents-lab/starter-code/app/src/main/java/co/ga/madlibs/MainActivity.java
@@ -2,11 +2,33 @@ package co.ga.madlibs;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        findViewById(R.id.submit_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+
+
+
+
+
+            }
+        });
+
+
     }
+
 }
