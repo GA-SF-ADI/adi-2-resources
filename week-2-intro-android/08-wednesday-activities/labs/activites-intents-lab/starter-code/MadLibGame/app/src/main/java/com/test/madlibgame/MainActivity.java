2fcc91e825d5b6a26a28f309cd54a39e586a5298
Sterling Gerritz <sterlinggerritz@Sterlings-MacBook-Air.local>
2016-06-16 00:08:03 -0700

    learned to pass a bundle.  had to create starter code from scratch due to 'sdk not found' issues

---
+++ week-2-intro-android/08-wednesday-activities/labs/activites-intents-lab/starter-code/MadLibGame/app/src/main/java/com/test/madlibgame/MainActivity.java
@@ -0,0 +1,59 @@
+package com.test.madlibgame;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Button getMadLib;
+    Intent sendToResultActivity;
+    private EditText adjective1MainEditText;
+    private EditText adjective2MainEditText;
+    private EditText noun1MainEditText;
+    private EditText noun2MainEditText;
+    private EditText animalsMainEditText;
+    private EditText gameMainEditText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getMadLib = (Button) findViewById(R.id.finished_button);
+        adjective1MainEditText = (EditText) findViewById(R.id.adjective1_xml);
+        adjective2MainEditText = (EditText) findViewById(R.id.adjective2_xml);
+        noun1MainEditText = (EditText) findViewById(R.id.noun1_xml);
+        noun2MainEditText = (EditText) findViewById(R.id.noun2_xml);
+        animalsMainEditText = (EditText) findViewById(R.id.animals_xml);
+        gameMainEditText = (EditText) findViewById(R.id.game_xml);
+
+        sendToResultActivity = new Intent(this, ResultActivity.class);
+
+        getMadLib.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Bundle bundle = new Bundle();
+                bundle.putString(ResultActivity.ADJECTIVE1, adjective1MainEditText.getText().toString());
+                bundle.putString(ResultActivity.ADJECTIVE2, adjective2MainEditText.getText().toString());
+                bundle.putString(ResultActivity.NOUN1, noun1MainEditText.getText().toString());
+                bundle.putString(ResultActivity.NOUN2, noun2MainEditText.getText().toString());
+                bundle.putString(ResultActivity.ANIMALS, animalsMainEditText.getText().toString());
+                bundle.putString(ResultActivity.GAME, gameMainEditText.getText().toString());
+
+
+                sendToResultActivity.putExtras(bundle);
+
+                startActivity(sendToResultActivity);
+            }
+        });
+
+
+    }
+}
---
+++ week-2-intro-android/08-wednesday-activities/lessons/activites-and-intents-lesson/starter-code/Addnumbers/app/src/main/java/com/test/addnumbers/MainActivity.java
@@ -0,0 +1,31 @@
+package com.test.addnumbers;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.mainButton);
+        assert button != null;
+        button.setOnClickListener(new View.OnClickListener() {
+                                     @Override
+                                     public void onClick(View v) {
+                                         Intent intent = new Intent(MainActivity.this, EchoActivity.class);
+                                         startActivity(intent);
+                                     }
+                                 }
+
+
+
+       );
+
+    }
+}
