38c901c8692e79004445edd01f297703508daa9e
langsmith <langstonlmcs@gmail.com>
2016-06-15 20:57:42 -0700

    working on put extras

--- week-2-intro-android/08-wednesday-activities/labs/activites-intents-lab/starter-code/app/src/main/java/co/ga/madlibs/MainActivity.java
+++ week-2-intro-android/08-wednesday-activities/labs/activites-intents-lab/starter-code/app/src/main/java/co/ga/madlibs/MainActivity.java
@@ -1,5 +1,6 @@
 package co.ga.madlibs;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -18,9 +19,45 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+//                words[0] = extras.getString(ADJECTIVE1, "???");
+//                words[1] = extras.getString(ADJECTIVE2, "???");
+//                words[2] = extras.getString(NOUN1, "???");
+//                words[3] = extras.getString(NOUN2, "???");
+//                words[4] = extras.getString(ANIMALS, "???");
+//                words[5] = extras.getString(GAME, "???");
+
+
+//                Intent intent = new Intent(this, DisplayMessageActivity.class);
+//                EditText editText = (EditText) findViewById(R.id.edit_message);
+//                String message = editText.getText().toString();
+//                intent.putExtra(EXTRA_MESSAGE, message);
+
+                EditText adjectiveone = (EditText) findViewById(R.id.adjective1_edittext);
+                EditText adjectivetwo = (EditText) findViewById(R.id.adjective2_edittext);
+                EditText nounone = (EditText) findViewById(R.id.noun1_edittext);
+                EditText nountwo = (EditText) findViewById(R.id.noun2_edittext);
+                EditText animal = (EditText) findViewById(R.id.animals_edittext);
+                EditText game = (EditText) findViewById(R.id.game_edittext);
+
+                String ad1 = adjectiveone.getText().toString();
+                String ad2 = adjectivetwo.getText().toString();
+                String noun1 = nounone.getText().toString();
+                String noun2 = nountwo.getText().toString();
+                String anim = animal.getText().toString();
+                String games = adjectiveone.getText().toString();
+
+                Intent intenttoResult = new Intent(MainActivity.this, ResultActivity.class);
+
+                intenttoResult.putExtra("Animal", ad1);
+                intenttoResult.putExtra("Animal", ad1);
+                intenttoResult.putExtra("Animal", ad1);
+                intenttoResult.putExtra("Animal", ad1);
+                intenttoResult.putExtra("Animal", ad1);
+                intenttoResult.putExtra("Animal", ad1);
 
 
 
+                startActivity(intenttoResult);
 
 
 
