017c77576b3c386d51366f36ec05552899916905
Vighnesh Ajay Rege <NehaRege@Nehas-MacBook-Pro.local>
2016-06-16 21:27:17 -0700

    Submitting week2 thursday homework Button Enabling

--- week-2-intro-android/09-thursday-lists/homework/event-handlers-lesson/starter-code/ButtonEnabling/app/src/main/java/ly/generalassemb/drewmahrt/buttonenabling/MainActivity.java
+++ week-2-intro-android/09-thursday-lists/homework/event-handlers-lesson/starter-code/ButtonEnabling/app/src/main/java/ly/generalassemb/drewmahrt/buttonenabling/MainActivity.java
@@ -2,6 +2,7 @@ package ly.generalassemb.drewmahrt.buttonenabling;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
@@ -26,5 +27,42 @@ public class MainActivity extends AppCompatActivity {
          * In the handler, disable the button using setEnabled(false). Pass in true to re-enable.
          */
 
+//        mAgreeCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//
+//            // we handle when we detect a change in checked state. Boolean isChecked tells us whether checkbox is checked or not.
+//            @Override
+//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//                // based on whether checkbox is checked or not, we enable/disable the button
+//                if(isChecked) {
+//                    mAgreeButton.setEnabled(true);
+//                } else {
+//                    mAgreeButton.setEnabled(false);
+//                }
+//            }
+//        });
+
+        CompoundButton.OnCheckedChangeListener buttonOnCheckedChangeListener = new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    mAgreeButton.setEnabled(true);
+                } else mAgreeButton.setEnabled(false);
+            }
+        };
+
+        mAgreeCheckBox.setOnCheckedChangeListener(buttonOnCheckedChangeListener);
+
+
+
+
+//        mAgreeCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener){
+//            @Override
+//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
+//                if(isChecked){
+//                    mAgreeButton.setEnabled(true);
+//                }
+//                else mAgreeButton.setEnabled(false);
+//            }
+//        });
     }
 }
