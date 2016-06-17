6eca4dc818ce6902ef17672e63b7bc0727b4f6fb
langsmith <langstonlmcs@gmail.com>
2016-06-17 11:27:27 -0700

    Finished getters and setters

--- week-2-intro-android/10-friday-classes/labs/classes-lab/starter-code/Classes-Lab/app/src/main/java/ly/generalassemb/drewmahrt/classes_lab/MainActivity.java
+++ week-2-intro-android/10-friday-classes/labs/classes-lab/starter-code/Classes-Lab/app/src/main/java/ly/generalassemb/drewmahrt/classes_lab/MainActivity.java
@@ -6,7 +6,10 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,30 +17,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Get output TextViews
-        TextView output = (TextView)findViewById(R.id.output_text);
-        TextView changedOutput = (TextView)findViewById(R.id.changed_output_text);
+        TextView output = (TextView) findViewById(R.id.output_text);
+        TextView changedOutput = (TextView) findViewById(R.id.changed_output_text);
 
         //Instantiate new Animal
-        Animal animal = ;
+        Animal animal = new Animal(4, 25, true, "Elephant");
+
 
         String name;
         int topSpeed;
 
         //get name and speed values using getters
-        name = ;
-        topSpeed = ;
+        name = animal.getName();
+
+        topSpeed = animal.getTopSpeed();
 
         //Set text of first TextView
-        output.setText();
+        output.setText("The " + name + " has a top speed of " + topSpeed + " mph");
 
         //Set new name, speed, and endangered properties values using setters
-        
+        animal.setName("Lion");
+        animal.setTopSpeed(50);
+        animal.setIsEndangered(false);
 
         //get new values using getters
-        name = ;
-        topSpeed = ;
+        name = animal.getName();
+        topSpeed = animal.getTopSpeed();
 
         //Set text of second TextView
-        changedOutput.setText();
+        changedOutput.setText("The " + name + " has a top speed of " + topSpeed + " mph");
     }
 }
