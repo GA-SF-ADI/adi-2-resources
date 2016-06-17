6894aaee05b08dd37d82c4e59835e4fba553c519
Patrick Cummins <patrickcummins2009@gmail.com>
2016-06-17 11:25:07 -0700

    AnimalLab

--- week-2-intro-android/10-friday-classes/labs/classes-lab/starter-code/Classes-Lab/app/src/main/java/ly/generalassemb/drewmahrt/classes_lab/Animal.java
+++ week-2-intro-android/10-friday-classes/labs/classes-lab/starter-code/Classes-Lab/app/src/main/java/ly/generalassemb/drewmahrt/classes_lab/Animal.java
@@ -8,6 +8,41 @@ public class Animal {
 
     public Animal(int numLegs, int topSpeed, boolean isEndangered, String name){
         //Set properties here
+        this.numLegs = numLegs;
+        this.topSpeed =topSpeed;
+        this.isEndangered = isEndangered;
+        this.name = name;
+    }
+
+    public void setNumLegs(int legs){
+        numLegs = legs;
+
+
+    }
+    public void setTopSpeed(int speed){
+        topSpeed=speed;
+    }
+    public void setEndangered(boolean endangered){
+        isEndangered = endangered;
+    }
+    public  void setName(String animalName){
+        name = animalName;
+    }
+
+    public int getNumLegs(){
+        return  numLegs;
+    }
+    public  int getTopSpeed(){
+        return topSpeed;
+    }
+    public boolean getEndangered(){
+        return isEndangered;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getAnimalNameandSpeedPrint(){
+        return "The " + name + " has a top speed of " + topSpeed + " mph!";
     }
 
     //Put getters and setters here
--- week-2-intro-android/10-friday-classes/labs/classes-lab/starter-code/Classes-Lab/app/src/main/java/ly/generalassemb/drewmahrt/classes_lab/MainActivity.java
+++ week-2-intro-android/10-friday-classes/labs/classes-lab/starter-code/Classes-Lab/app/src/main/java/ly/generalassemb/drewmahrt/classes_lab/MainActivity.java
@@ -18,26 +18,28 @@ public class MainActivity extends AppCompatActivity {
         TextView changedOutput = (TextView)findViewById(R.id.changed_output_text);
 
         //Instantiate new Animal
-        Animal animal = ;
+        Animal animal = new Animal(4,20, false, "Dog");
 
         String name;
         int topSpeed;
 
         //get name and speed values using getters
-        name = ;
-        topSpeed = ;
+        name = animal.getName();
+        topSpeed = animal.getTopSpeed();
 
         //Set text of first TextView
-        output.setText();
+        output.setText(animal.getAnimalNameandSpeedPrint());
 
         //Set new name, speed, and endangered properties values using setters
-        
+        animal.setName("Cheetah");
+        animal.setEndangered(false);
+        animal.setTopSpeed(75);
 
         //get new values using getters
-        name = ;
-        topSpeed = ;
+        name = animal.getName();
+        topSpeed = animal.getTopSpeed();
 
         //Set text of second TextView
-        changedOutput.setText();
+        changedOutput.setText(animal.getAnimalNameandSpeedPrint());
     }
 }
