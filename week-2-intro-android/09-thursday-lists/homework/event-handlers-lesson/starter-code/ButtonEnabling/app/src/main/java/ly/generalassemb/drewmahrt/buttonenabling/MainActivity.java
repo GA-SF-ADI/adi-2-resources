8b49a0190c7c0f997cc984e2e8adc1f95bb93ff9
langsmith <langstonlmcs@gmail.com>
2016-06-16 21:49:19 -0700

    Almost done. I can check the box off and on.

--- week-2-intro-android/09-thursday-lists/homework/event-handlers-lesson/starter-code/ButtonEnabling/app/src/main/java/ly/generalassemb/drewmahrt/buttonenabling/MainActivity.java
+++ week-2-intro-android/09-thursday-lists/homework/event-handlers-lesson/starter-code/ButtonEnabling/app/src/main/java/ly/generalassemb/drewmahrt/buttonenabling/MainActivity.java
@@ -2,10 +2,12 @@ package ly.generalassemb.drewmahrt.buttonenabling;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     CheckBox mAgreeCheckBox;
@@ -16,8 +18,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mAgreeCheckBox = (CheckBox)findViewById(R.id.agree_check_box);
-        mAgreeButton = (Button)findViewById(R.id.agree_button);
+        mAgreeCheckBox = (CheckBox) findViewById(R.id.agree_check_box);
+        mAgreeButton = (Button) findViewById(R.id.agree_button);
 
         //Call setOnCheckedChangeListener on the mAgreeCheckBox, and complete the listener
         /**
@@ -30,12 +32,21 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
 
+                if (isChecked) {
 
+                    mAgreeButton.setEnabled(true);
+                }else {
 
+                Toast.makeText(MainActivity.this, "test message", Toast.LENGTH_SHORT).show();
 
             }
-        });
 
+
+        }
+
+
+    });
 
     }
+
 }
