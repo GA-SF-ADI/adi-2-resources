ede71026958b245c359bd06c58c9802e83a2127f
Nolberto Arroyo <nolberto.arroyo@gmail.com>
2016-06-17 12:03:54 -0700

    classes lab

--- week-2-intro-android/10-friday-classes/labs/classes-lab/starter-code/Classes-Lab/app/src/main/java/ly/generalassemb/drewmahrt/classes_lab/Animal.java
+++ week-2-intro-android/10-friday-classes/labs/classes-lab/starter-code/Classes-Lab/app/src/main/java/ly/generalassemb/drewmahrt/classes_lab/Animal.java
@@ -7,8 +7,36 @@ public class Animal {
     String name;
 
     public Animal(int numLegs, int topSpeed, boolean isEndangered, String name){
-        //Set properties here
+
+        this.numLegs = numLegs;
+        this.topSpeed = topSpeed;
+        this.isEndangered = isEndangered;
+        this.name = name;
+
+    }//created setters and getters to set and get variables
+    public int getNumLegs(){
+        return numLegs;
+    }
+    public void setNumLegs(int numLegs){
+        this.numLegs=numLegs;
+    }
+    public int getTopSpeed(){
+        return topSpeed;
+    }
+    public void setTopSpeed(int topSpeed){
+        this.topSpeed=topSpeed;
+    }
+    public boolean isEndangered(){
+        return isEndangered;
+    }
+    public void setisEndangered(boolean endangered ){
+        this.isEndangered=endangered;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name=name;
     }
 
-    //Put getters and setters here
 }
