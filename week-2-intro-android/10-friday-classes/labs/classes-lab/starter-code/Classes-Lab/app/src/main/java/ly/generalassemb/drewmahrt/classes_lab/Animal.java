37426078db04ec5a7b14aeb06378d59310ff1b47
Vighnesh Ajay Rege <NehaRege@Nehas-MacBook-Pro.local>
2016-06-17 11:38:42 -0700

    Submitting week2-friday_Basic Classes_Lab

--- week-2-intro-android/10-friday-classes/labs/classes-lab/starter-code/Classes-Lab/app/src/main/java/ly/generalassemb/drewmahrt/classes_lab/Animal.java
+++ week-2-intro-android/10-friday-classes/labs/classes-lab/starter-code/Classes-Lab/app/src/main/java/ly/generalassemb/drewmahrt/classes_lab/Animal.java
@@ -1,14 +1,52 @@
 package ly.generalassemb.drewmahrt.classes_lab;
 
-public class Animal {
+public class Animal{
     int numLegs;
     int topSpeed;
     boolean isEndangered;
     String name;
 
-    public Animal(int numLegs, int topSpeed, boolean isEndangered, String name){
-        //Set properties here
+    public Animal(int numLegs,int topSpeed,boolean isEndangered,String name){
+        this.numLegs = numLegs;
+        this.topSpeed = topSpeed;
+        this.isEndangered = isEndangered;
+        this.name = name;
     }
 
-    //Put getters and setters here
+    public int getNumLegs(){
+        return numLegs;
+    }
+
+    public int getTopSpeed(){
+        return topSpeed;
+    }
+
+    public boolean isEndangered(){
+        return isEndangered;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setNumLegs(int numLegs){
+        this.numLegs=numLegs;
+    }
+
+    public void setTopSpeed(int topSpeed){
+        this.topSpeed=topSpeed;
+    }
+
+    public  void setIsEndangered(boolean isEndangered){
+        this.isEndangered=isEndangered;
+    }
+
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public String printMyAnimalProperties(Animal animal) {
+
+        return "Animal name is : " + name+". It has "+numLegs+" legs and a speed of "+topSpeed+" mph";
+    }
 }
--- week-2-intro-android/10-friday-classes/labs/classes-lab/starter-code/Classes-Lab/app/src/main/java/ly/generalassemb/drewmahrt/classes_lab/MainActivity.java
+++ week-2-intro-android/10-friday-classes/labs/classes-lab/starter-code/Classes-Lab/app/src/main/java/ly/generalassemb/drewmahrt/classes_lab/MainActivity.java
@@ -16,28 +16,35 @@ public class MainActivity extends AppCompatActivity {
         //Get output TextViews
         TextView output = (TextView)findViewById(R.id.output_text);
         TextView changedOutput = (TextView)findViewById(R.id.changed_output_text);
+        TextView bonusOutput = (TextView) findViewById(R.id.bonus_output_text);
 
         //Instantiate new Animal
-        Animal animal = ;
+        Animal animal = new Animal(4,20,false,"elephant");
 
         String name;
         int topSpeed;
 
         //get name and speed values using getters
-        name = ;
-        topSpeed = ;
+        name = animal.getName();
+        topSpeed = animal.getTopSpeed() ;
 
         //Set text of first TextView
-        output.setText();
+        output.setText("The "+name+" has a top speed of "+topSpeed+" mph");
 
         //Set new name, speed, and endangered properties values using setters
-        
+        animal.setName("lion");
+        animal.setTopSpeed(50);
+        animal.setIsEndangered(false);
 
         //get new values using getters
-        name = ;
-        topSpeed = ;
+        name = animal.getName();
+        topSpeed = animal.getTopSpeed() ;
 
         //Set text of second TextView
-        changedOutput.setText();
+        changedOutput.setText("The "+name+" has a top speed of "+topSpeed+" mph");
+
+        //bonus exercise
+
+        bonusOutput.setText(animal.printMyAnimalProperties(animal));
     }
 }
