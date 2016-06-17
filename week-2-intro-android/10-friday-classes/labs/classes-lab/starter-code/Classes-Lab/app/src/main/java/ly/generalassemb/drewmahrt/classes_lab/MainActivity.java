187c05c1dc34ce3584f07cb5388e2381bd437ee5
langsmith <langstonlmcs@gmail.com>
2016-06-17 15:44:58 -0700

    Final layout done. Setting up capturing editText data

--- week-2-intro-android/10-friday-classes/labs/classes-lab/starter-code/Classes-Lab/app/src/main/java/ly/generalassemb/drewmahrt/classes_lab/MainActivity.java
+++ week-2-intro-android/10-friday-classes/labs/classes-lab/starter-code/Classes-Lab/app/src/main/java/ly/generalassemb/drewmahrt/classes_lab/MainActivity.java
@@ -3,7 +3,10 @@ package ly.generalassemb.drewmahrt.classes_lab;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 
         //Get output TextViews
         TextView output = (TextView) findViewById(R.id.output_text);
@@ -51,13 +56,39 @@ public class MainActivity extends AppCompatActivity {
         changedOutput.setText("The " + name + " has a top speed of " + topSpeed + " mph");
 
         Button addMammalButton = (Button) findViewById(R.id.mammal_button);
-
-        Toast.makeText(MainActivity.this, "Mammal button's been clicked", Toast.LENGTH_SHORT).show();
-
-
         Button addReptileButton = (Button) findViewById(R.id.reptile_button);
 
-        Toast.makeText(MainActivity.this, "Reptile button's been clicked", Toast.LENGTH_SHORT).show();
+
+//        mammal, offspring, reptile name, scales
+
+
+        EditText mammalName = (EditText) findViewById(R.id.mammal_name);
+        Integer numOfMammalOffSpring = (Integer) findViewById(R.id.number_of_offspring);
+        EditText reptileName = (EditText) findViewById(R.id.reptile_name);
+        Integer numOfReptileScales = (Integer) findViewById(R.id.number_of_scales);
+
+        Integer reptileScaleNum = parseInt(numOfReptileScales)
+
+        addMammalButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(MainActivity.this, "Mammal button clicked", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+        addReptileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(MainActivity.this, "Reptile button clicked", Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+
 
     }
 }
