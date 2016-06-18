7d7655b94d5d781f840a403d25b1af406421027c
langsmith <langstonlmcs@gmail.com>
2016-06-17 21:12:33 -0700

    Added subclasses of mammal and reptile classes

---
+++ week-2-intro-android/10-friday-classes/labs/classes-lab/starter-code/Classes-Lab/app/src/main/java/ly/generalassemb/drewmahrt/classes_lab/DiamondbackSnake.java
@@ -0,0 +1,33 @@
+package ly.generalassemb.drewmahrt.classes_lab;
+
+/**
+ * Created by LangstonSmith on 6/17/16.
+ */
+public class DiamondbackSnake extends Reptile{
+
+    String tongueFlick;
+
+    String rattler;
+
+    public DiamondbackSnake(int numLegs, int topSpeed, boolean isEndangered, String name, int eggs, String scales, String tongueFlick, String rattler) {
+        super(numLegs, topSpeed, isEndangered, name, eggs, scales);
+        this.tongueFlick = tongueFlick;
+        this.rattler = rattler;
+    }
+
+    public String getTongueFlick() {
+        return tongueFlick;
+    }
+
+    public void setTongueFlick(String tongueFlick) {
+        this.tongueFlick = tongueFlick;
+    }
+
+    public String getRattler() {
+        return rattler;
+    }
+
+    public void setRattler(String rattler) {
+        this.rattler = rattler;
+    }
+}
