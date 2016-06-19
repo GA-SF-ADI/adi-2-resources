5c638956d672a2494181421a2d9b2e8874542a8e
langsmith <langstonlmcs@gmail.com>
2016-06-18 23:36:29 -0700

    Finished creating Android phone book class

---
+++ week-2-intro-android/10-friday-classes/homework/classes/ClassesDay10WeekendHomework/app/src/main/java/com/test/classesday10weekendhomework/AndroidPhoneBook.java
@@ -0,0 +1,41 @@
+package com.test.classesday10weekendhomework;
+
+import java.util.List;
+
+/**
+ * Created by LangstonSmith on 6/18/16.
+ */
+public class AndroidPhoneBook extends PhoneBook {
+
+
+    private float androidVersion;
+
+    public AndroidPhoneBook(List<Contact> listOfContacts, float androidVersion) {
+        super(listOfContacts);
+        this.androidVersion = androidVersion;
+    }
+
+
+    public float getAndroidVersion() {
+        return androidVersion;
+    }
+
+    public void setAndroidVersion(float androidVersion) {
+        this.androidVersion = androidVersion;
+    }
+
+    public boolean checkIfAndroidVersionIsSupported() {
+
+        if (androidVersion > 4.0f) {
+
+            return true;
+
+        }
+
+
+
+        
+    }
+
+
+}
