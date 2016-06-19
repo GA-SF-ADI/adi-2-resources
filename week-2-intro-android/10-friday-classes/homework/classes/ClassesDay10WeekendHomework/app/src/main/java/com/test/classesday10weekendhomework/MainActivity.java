f9f526685e10e2551e5d30b66c0b8dd9d2f88964
langsmith <langstonlmcs@gmail.com>
2016-06-18 21:52:23 -0700

    Creating main class instances

--- week-2-intro-android/10-friday-classes/homework/classes/ClassesDay10WeekendHomework/app/src/main/java/com/test/classesday10weekendhomework/MainActivity.java
+++ week-2-intro-android/10-friday-classes/homework/classes/ClassesDay10WeekendHomework/app/src/main/java/com/test/classesday10weekendhomework/MainActivity.java
@@ -9,5 +9,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    PhoneBook iPhone = new PhoneBook()
+
+
+
+
+
+
+
+
+
+
     }
 }
--- week-2-intro-android/10-friday-classes/homework/classes/ClassesDay10WeekendHomework/app/src/main/java/com/test/classesday10weekendhomework/PhoneBook.java
+++ week-2-intro-android/10-friday-classes/homework/classes/ClassesDay10WeekendHomework/app/src/main/java/com/test/classesday10weekendhomework/PhoneBook.java
@@ -1,7 +1,25 @@
 package com.test.classesday10weekendhomework;
 
+import java.util.ArrayList;
+
 /**
  * Created by LangstonSmith on 6/18/16.
  */
 public class PhoneBook {
+
+
+    private ArrayList contacts;
+
+    public PhoneBook(ArrayList contacts) {
+        this.contacts = contacts;
+
+    }
+
+    public PhoneBook() {
+
+    }
+
+    
+
+
 }
