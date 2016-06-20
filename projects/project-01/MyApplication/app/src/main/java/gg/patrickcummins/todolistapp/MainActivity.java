55845af539495927190eafdc4f88909cc891c582
Patrick Cummins <patrickcummins2009@gmail.com>
2016-06-20 09:58:09 -0700

    Beginning of Project 1

---
+++ projects/project-01/MyApplication/app/src/main/java/gg/patrickcummins/todolistapp/MainActivity.java
@@ -0,0 +1,13 @@
+package gg.patrickcummins.todolistapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
