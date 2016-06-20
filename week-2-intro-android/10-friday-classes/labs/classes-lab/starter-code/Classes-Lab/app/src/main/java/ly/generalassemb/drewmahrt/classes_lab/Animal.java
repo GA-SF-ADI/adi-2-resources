7d47661b55ef8a8cceec61de9cc46422306d03e4
Michael Jeffress <michaeljeffress@Michaels-MacBook-Pro.local>
2016-06-20 08:56:46 -0700

    lab work

--- week-2-intro-android/10-friday-classes/labs/classes-lab/starter-code/Classes-Lab/app/src/main/java/ly/generalassemb/drewmahrt/classes_lab/Animal.java
+++ week-2-intro-android/10-friday-classes/labs/classes-lab/starter-code/Classes-Lab/app/src/main/java/ly/generalassemb/drewmahrt/classes_lab/Animal.java
@@ -1,14 +1,47 @@
 package ly.generalassemb.drewmahrt.classes_lab;
 
-public class Animal {
-    int numLegs;
-    int topSpeed;
-    boolean isEndangered;
-    String name;
+/**
+ * Created by michaeljeffress on 6/19/16.
+ */
+import java.io.Serializable;
 
-    public Animal(int numLegs, int topSpeed, boolean isEndangered, String name){
-        //Set properties here
+public class Animal implements Serializable {
+
+    // This static variable is required for Serializable to work. Don't worry about it for now.
+    private static final long serialVersionUID = 7526472295622776147L;
+
+    int mTopSpeed;
+    boolean mIsEndangered;
+    String mName;
+
+    public Animal(int topSpeed, boolean isEndangered, String name){
+        this.mTopSpeed = topSpeed;
+        this.mIsEndangered = isEndangered;
+        this.mName = name;
     }
 
-    //Put getters and setters here
+    public int getTopSpeed() {
+        return mTopSpeed;
+    }
+
+    public void setTopSpeed(int topSpeed) {
+        this.mTopSpeed = topSpeed;
+    }
+
+    public boolean isEndangered() {
+        return mIsEndangered;
+    }
+
+    public void setIsEndangered(boolean isEndangered) {
+        this.mIsEndangered = isEndangered;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        this.mName = name;
+    }
 }
+
