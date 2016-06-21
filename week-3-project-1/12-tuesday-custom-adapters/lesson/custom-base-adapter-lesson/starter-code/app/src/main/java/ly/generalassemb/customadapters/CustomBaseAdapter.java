b540b9f558e35e2b8a19e2147b39729e05a8df9c
Aleksandr Tomak <alex@Aleksandrs-MacBook-Pro.local>
2016-06-20 22:47:12 -0700

    Add custom adapter lesson

---
+++ week-3-project-1/12-tuesday-custom-adapters/lesson/custom-base-adapter-lesson/starter-code/app/src/main/java/ly/generalassemb/customadapters/CustomBaseAdapter.java
@@ -0,0 +1,32 @@
+package ly.generalassemb.customadapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+/**
+ * Custom Adapter for our two TextViews and single Button.
+ *
+ * It holds a list of Animals and binds the animal name to
+ * firstTextView, animal sound to secondTextView.
+ *
+ * When button is clicked a toast is made with animal name saying
+ * animal sound ( i.e. cow says moo ).
+ */
+public class CustomBaseAdapter extends BaseAdapter {
+
+    // TODO: implement the rest of the CustomBaseAdapter class
+
+    // hint: Create constructor first. Implement 4 methods next.
+
+
+
+    // TODO: Bonus - use a ViewHolder!
+}
