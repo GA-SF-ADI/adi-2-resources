8933f62d59209afedc888669e981995403316330
Patrick Cummins <patrickcummins2009@gmail.com>
2016-06-21 11:36:14 -0700

    classWork

--- week-3-project-1/12-tuesday-custom-adapters/lesson/custom-base-adapter-lesson/starter-code/app/src/main/java/ly/generalassemb/customadapters/CustomBaseAdapter.java
+++ week-3-project-1/12-tuesday-custom-adapters/lesson/custom-base-adapter-lesson/starter-code/app/src/main/java/ly/generalassemb/customadapters/CustomBaseAdapter.java
@@ -21,12 +21,71 @@ import java.util.ArrayList;
  * animal sound ( i.e. cow says moo ).
  */
 public class CustomBaseAdapter extends BaseAdapter {
+    private ArrayList<Animal> data;
+    private Context context;
+    private ViewHolder viewHolder;
 
-    // TODO: implement the rest of the CustomBaseAdapter class
+    public CustomBaseAdapter(Context context, ArrayList<Animal> animalList) {
+        this.data = animalList;
+        this.context = context;
 
-    // hint: Create constructor first. Implement 4 methods next.
+    }
+
+    @Override
+    public int getCount() {
+        return data.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return data.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        if (convertView == null){
+            convertView = LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
+            viewHolder = new ViewHolder(convertView);
+            convertView.setTag(viewHolder);
+        }else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        TextView textView1 = viewHolder.firstTV;
+        TextView textView2 = viewHolder.secondTV;
+        Button mButton = viewHolder.theButton;
+
+        final Animal currentAnimal = data.get(position);
+
+        textView1.setText(currentAnimal.getName());
+        textView2.setText(currentAnimal.getSound());
+
+        mButton.setOnClickListener(new android.view.View.OnClickListener() {
+            @Override
+            public void onClick(android.view.View v) {
+                Toast.makeText(context, "The " + currentAnimal.getName() + " goes " + currentAnimal.getSound(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return convertView;
+    }
+    private class ViewHolder{
+        TextView firstTV;
+        TextView secondTV;
+        Button theButton;
+
+        public ViewHolder(View itemLayout) {
+            this.firstTV = (TextView) itemLayout.findViewById(R.id.list_item_tv_first);
+            this.secondTV = (TextView) itemLayout.findViewById(R.id.list_item_tv_second);
+            this.theButton = (Button) itemLayout.findViewById(R.id.list_item_button);
+        }
+    }
 
 
-
-    // TODO: Bonus - use a ViewHolder!
 }
