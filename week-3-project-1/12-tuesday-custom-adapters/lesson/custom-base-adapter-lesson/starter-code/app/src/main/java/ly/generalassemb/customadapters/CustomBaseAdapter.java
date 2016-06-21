b3dbb132ec067dd1c796db1d3e296a87db4527e0
Kitty Tang <Kittys.Tang@gmail.com>
2016-06-21 11:11:00 -0700

    added custom adapter

--- week-3-project-1/12-tuesday-custom-adapters/lesson/custom-base-adapter-lesson/starter-code/app/src/main/java/ly/generalassemb/customadapters/CustomBaseAdapter.java
+++ week-3-project-1/12-tuesday-custom-adapters/lesson/custom-base-adapter-lesson/starter-code/app/src/main/java/ly/generalassemb/customadapters/CustomBaseAdapter.java
@@ -26,7 +26,54 @@ public class CustomBaseAdapter extends BaseAdapter {
 
     // hint: Create constructor first. Implement 4 methods next.
 
+    private ArrayList<Animal> data;
+    private Context context;
 
+    public CustomBaseAdapter(Context context, ArrayList<Animal> animalList) {
+        this.data = animalList;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return data.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return data.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if(convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
+        }
+
+        TextView firstTV = (TextView) convertView.findViewById(R.id.list_item_tv_first);
+        TextView secondTV = (TextView) convertView.findViewById(R.id.list_item_tv_second);
+        Button button = (Button) convertView.findViewById(R.id.list_item_button);
+
+        final Animal currentAnimal = data.get(position);
+
+        firstTV.setText(currentAnimal.getName());
+        secondTV.setText(currentAnimal.getSound());
+
+        final String toastText = currentAnimal.getName() + " says: " + currentAnimal.getSound();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, toastText, Toast.LENGTH_SHORT).show();
+            }
+        });
+        return convertView;
+    }
 
     // TODO: Bonus - use a ViewHolder!
 }
