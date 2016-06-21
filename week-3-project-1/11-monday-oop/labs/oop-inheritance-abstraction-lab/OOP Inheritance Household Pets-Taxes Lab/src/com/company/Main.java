494f249cad758a0fc4680bd9e93eda078b8605fe
langsmith <langstonlmcs@gmail.com>
2016-06-20 19:26:12 -0700

    Cleaned up main method area

--- week-3-project-1/11-monday-oop/labs/oop-inheritance-abstraction-lab/OOP Inheritance Household Pets-Taxes Lab/src/com/company/Main.java
+++ week-3-project-1/11-monday-oop/labs/oop-inheritance-abstraction-lab/OOP Inheritance Household Pets-Taxes Lab/src/com/company/Main.java
@@ -6,9 +6,14 @@ public class Main {
 
     public static void main(String[] args) {
 
+//        Declaring class variables for printing below
+
         DailyPetCare caringForPet = new DailyPetCare();
 
         PayingTax taxPayment = new PayingTax(.8, 39329);
+
+
+//        Printing out lines
 
 
         System.out.println(caringForPet.playWithPet());
