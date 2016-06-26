97239549531abd329a143895c33b9437c6175978
langsmith <langstonlmcs@gmail.com>
2016-06-26 09:55:11 -0700

    working on main activity

--- week-3-project-1/15-friday/homework/2ndPandoraAttempt/2ndPandoraAttempt/app/src/main/java/com/test/a2ndpandoraattempt/MainActivity.java
+++ week-3-project-1/15-friday/homework/2ndPandoraAttempt/2ndPandoraAttempt/app/src/main/java/com/test/a2ndpandoraattempt/MainActivity.java
@@ -3,30 +3,24 @@ package com.test.a2ndpandoraattempt;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activ);
+        setContentView(R.layout.activity_main);
 
+        Random ran = new Random();
+        int randomNum = ran.nextInt(2016 - 2010) + 2010;
 
         Station stationObject = new Station("Prince", 2015);
 
 
-        
+        stationObject.setYearWhenListenedTo(randomNum);
 
-
-
-
-
-
-
-
-
-
-
-
+        CustomStationAdapter stationAdapter
 
     }
 }
