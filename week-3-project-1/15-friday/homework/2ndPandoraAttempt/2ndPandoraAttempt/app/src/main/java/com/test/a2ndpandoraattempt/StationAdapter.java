fd2669c49c6c7069af1f8c347828abcb305cf6c3
langsmith <langstonlmcs@gmail.com>
2016-06-25 21:05:34 -0700

    setting up adapter

--- week-3-project-1/15-friday/homework/2ndPandoraAttempt/2ndPandoraAttempt/app/src/main/java/com/test/a2ndpandoraattempt/StationAdapter.java
+++ week-3-project-1/15-friday/homework/2ndPandoraAttempt/2ndPandoraAttempt/app/src/main/java/com/test/a2ndpandoraattempt/StationAdapter.java
@@ -1,7 +1,32 @@
 package com.test.a2ndpandoraattempt;
 
+import android.view.View;
+import android.widget.TextView;
+
 /**
  * Created by LangstonSmith on 6/25/16.
  */
 public class StationAdapter {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- week-3-project-1/15-friday/homework/2ndPandoraAttempt/2ndPandoraAttempt/app/src/main/java/com/test/a2ndpandoraattempt/StationObject.java
+++ week-3-project-1/15-friday/homework/2ndPandoraAttempt/2ndPandoraAttempt/app/src/main/java/com/test/a2ndpandoraattempt/StationObject.java
@@ -4,4 +4,31 @@ package com.test.a2ndpandoraattempt;
  * Created by LangstonSmith on 6/25/16.
  */
 public class StationObject {
+
+    String stationName;
+
+    Integer yearWhenListenedTo;
+
+
+    public StationObject(String stationName, Integer yearWhenListenedTo) {
+        this.stationName = stationName;
+        this.yearWhenListenedTo = yearWhenListenedTo;
+    }
+
+
+    public String getStationName() {
+        return stationName;
+    }
+
+    public void setStationName(String stationName) {
+        this.stationName = stationName;
+    }
+
+    public Integer getYearWhenListenedTo() {
+        return yearWhenListenedTo;
+    }
+
+    public void setYearWhenListenedTo(Integer yearWhenListenedTo) {
+        this.yearWhenListenedTo = yearWhenListenedTo;
+    }
 }
