030bcaafce2a868a96c2404472870d93fa8662aa
langsmith <langstonlmcs@gmail.com>
2016-06-26 10:06:56 -0700

    filling out main activity tab

--- week-3-project-1/15-friday/homework/2ndPandoraAttempt/2ndPandoraAttempt/app/src/main/java/com/test/a2ndpandoraattempt/MainActivity.java
+++ week-3-project-1/15-friday/homework/2ndPandoraAttempt/2ndPandoraAttempt/app/src/main/java/com/test/a2ndpandoraattempt/MainActivity.java
@@ -3,7 +3,10 @@ package com.test.a2ndpandoraattempt;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,16 +16,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button addStationButton = (Button) findViewById(R.id.button_add_station);
+
+        ListView listOfStations = (ListView) findViewById(R.id.listView_list_of_stations);
+
+        ArrayList<Station> arrayOfStations = new ArrayList<>();
 
         Random ran = new Random();
         int randomNum = ran.nextInt(2016 - 2010) + 2010;
 
         Station stationObject = new Station("Prince", 2015);
 
-
         stationObject.setYearWhenListenedTo(randomNum);
 
-        CustomStationAdapter stationAdapter
+        CustomStationAdapter stationAdapter = new CustomStationAdapter(MainActivity.this, )
 
     }
 }
