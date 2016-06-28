8db2ff723465f61c7867c1feb19de94cae40005b
Nolberto Arroyo <nolberto.arroyo@gmail.com>
2016-06-28 11:00:38 -0700

    tuesday independent practice

--- week-4-databases/17-tuesday-cursors/lessons/sqlite-in-android/starter-code/SQLiteAndroidPractice/app/src/main/java/com/example/jeanweatherwax/sqliteandroidpractice/MainActivity.java
+++ week-4-databases/17-tuesday-cursors/lessons/sqlite-in-android/starter-code/SQLiteAndroidPractice/app/src/main/java/com/example/jeanweatherwax/sqliteandroidpractice/MainActivity.java
@@ -9,5 +9,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // created an instance of databasehelper
+        DatabaseHelper db = new DatabaseHelper(this);
+        //used the insert method from databasehelper to insert values in database
+        db.insert(1, "Super Mario", "1988");
+        db.insert(2,"Lu", "1986");
+
+        //returning value at id 2
+        Game retrievedGame = db.getGame(2);
     }
 }
