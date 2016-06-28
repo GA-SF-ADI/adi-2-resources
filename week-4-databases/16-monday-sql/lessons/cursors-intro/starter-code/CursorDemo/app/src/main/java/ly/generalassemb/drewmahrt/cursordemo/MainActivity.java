da9d536ee66702157dace474fb1ef70dbeb74b28
Kitty Tang <Kittys.Tang@gmail.com>
2016-06-27 19:34:47 -0700

    lesson 16 in class lab

--- week-4-databases/16-monday-sql/lessons/cursors-intro/starter-code/CursorDemo/app/src/main/java/ly/generalassemb/drewmahrt/cursordemo/MainActivity.java
+++ week-4-databases/16-monday-sql/lessons/cursors-intro/starter-code/CursorDemo/app/src/main/java/ly/generalassemb/drewmahrt/cursordemo/MainActivity.java
@@ -1,8 +1,10 @@
 package ly.generalassemb.drewmahrt.cursordemo;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import java.util.Locale;
 
@@ -25,11 +27,21 @@ public class MainActivity extends AppCompatActivity {
         db.setVersion(1);
         db.setLocale(Locale.getDefault());
 
-        String[] countryNames = new String[]{"Cananda","USA","Mexico"};
+        String[] countryNames = new String[]{"Canada","USA","Mexico"};
         int[] populations = new int[]{35,318,125};
+
         db.execSQL("CREATE TABLE IF NOT EXISTS tbl_countries (country_name VARCHAR, population VARCHAR);");
+
         for (int i=0; i<countryNames.length;i++) {
             db.execSQL("INSERT INTO tbl_countries Values ('" + countryNames[i] + "', '"+ populations[i] + "');");
+        }
+
+        Cursor cursor = db.query("tbl_countries",null, null, null, null, null, null);
+
+        cursor.moveToFirst();
+        while (cursor.isAfterLast() == false) {
+            Log.d(TAG,"DB result "+cursor.getString(cursor.getColumnIndex("country_name"))+": "+cursor.getString(cursor.getColumnIndex("population")));
+            cursor.moveToNext();
         }
 
     }
--- week-4-databases/16-monday-sql/lessons/cursors-intro/starter-code/CursorExercise/app/src/main/java/ly/generalassemb/drewmahrt/cursorlab/MainActivity.java
+++ week-4-databases/16-monday-sql/lessons/cursors-intro/starter-code/CursorExercise/app/src/main/java/ly/generalassemb/drewmahrt/cursorlab/MainActivity.java
@@ -1,9 +1,14 @@
 package ly.generalassemb.drewmahrt.cursorlab;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import java.util.ArrayList;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
@@ -35,27 +40,33 @@ public class MainActivity extends AppCompatActivity {
          * Start your code here
          */
         // 1. Get your cursor
-
+        Cursor cursor = db.query("tbl_books",null, null, null, null, null, null);
 
 
         // 2. Iterate on the cursor and create a string for each row that looks like "Title - author - year"
+        ArrayList<String> arrayList = new ArrayList<>();
+        cursor.moveToFirst();
 
-
+        while (cursor.isAfterLast() == false) {
+            arrayList.add(cursor.getString(cursor.getColumnIndex("title")) + " - " + cursor.getString(cursor.getColumnIndex("author")) + " - " + cursor.getString(cursor.getColumnIndex("year")));
+            cursor.moveToNext();
+        }
+        cursor.close();
 
         // 3. Insert each of the strings from step 2 into an ArrayList<String>
 
 
 
         // 4. Create ListView
-
+        ListView listView = (ListView) findViewById(R.id.book_list);
 
 
         // 5. Create adapter for listview ( ArrayAdapter or CustomAdapter )
+        ArrayAdapter<String> mArraryAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1 ,arrayList);
 
 
-        
         // 6. Link your ListView and Adapter
-
+        listView.setAdapter(mArraryAdapter);
 
     }
 }
