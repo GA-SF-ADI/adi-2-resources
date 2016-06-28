52e74f0a7725e146e4e65c5acbfee1d74a799108
langsmith <langstonlmcs@gmail.com>
2016-06-28 16:16:26 -0700

    finished lesson

--- week-4-databases/17-tuesday-cursors/labs/cursor-adapters-lab/starter-code/ShoppingListVer2/app/src/main/java/ly/generalassemb/drewmahrt/shoppinglistver2/MainActivity.java
+++ week-4-databases/17-tuesday-cursors/labs/cursor-adapters-lab/starter-code/ShoppingListVer2/app/src/main/java/ly/generalassemb/drewmahrt/shoppinglistver2/MainActivity.java
@@ -9,6 +9,9 @@ import android.view.ViewGroup;
 import android.widget.CursorAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+
+import java.util.List;
 
 import ly.generalassemb.drewmahrt.shoppinglistver2.setup.DBAssetHelper;
 
@@ -21,6 +24,32 @@ public class MainActivity extends AppCompatActivity {
         //Ignore the two lines below, they are for setup
         DBAssetHelper dbSetup = new DBAssetHelper(MainActivity.this);
         dbSetup.getReadableDatabase();
+
+
+        ListView listView = (ListView) findViewById(R.id.shopping_list_view);
+
+        DBAssetHelper adapter = DBAssetHelper.getInstance(this);
+
+
+        Cursor cursor = adapter.getShoppingList();
+
+
+        CursorAdapter mCursorAdapter = new CursorAdapter(MainActivity.this, cursor, 0) {
+            @Override
+            public View newView(Context context, Cursor cursor, ViewGroup parent) {
+                return null;
+            }
+
+            @Override
+            public void bindView(View view, Context context, Cursor cursor) {
+
+                TextView itemListView = (TextView) findViewById(R.id.textView_single_item);
+                itemListView.setText(cursor.getString(cursor.getColumnIndex(DBAssetHelper.COL_ITEM_NAME)));
+
+
+            }
+        };
+
 
     }
 }
