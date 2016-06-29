06f402d33e7c8353f524de59b8ead249290ec693
langsmith <langstonlmcs@gmail.com>
2016-06-28 23:34:57 -0700

    almost done

--- week-4-databases/17-tuesday-cursors/labs/ExtraCursorAdapterPractice/starter-code/app/src/main/java/com/example/hollis/cursoradapterexample/AnimalSQLOpenHelper.java
+++ week-4-databases/17-tuesday-cursors/labs/ExtraCursorAdapterPractice/starter-code/app/src/main/java/com/example/hollis/cursoradapterexample/AnimalSQLOpenHelper.java
@@ -1,5 +1,8 @@
 package com.example.hollis.cursoradapterexample;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 /**
@@ -7,45 +10,75 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 public class AnimalSQLOpenHelper extends SQLiteOpenHelper {
 
+
+    public static final String TAG = "AnimalSqlOpenHelper";
+
+    public static AnimalSQLOpenHelper curInstance;
+
     private static String DATABASE_NAME = "animal_db";
     private static final int DATABASE_VERSION = 7;
+    public static final String TABLE_NAME = "animals";
+    private static String COL_ID = "_id";
     private static String COL_NAME = "name";
     private static String COL_SOUND = "sound";
     private static String COL_DESCRIPTION = "description";
 
+    public static final String[] ANIMAL_COLUMNS = {COL_ID, COL_NAME, COL_SOUND, COL_DESCRIPTION};
 
-    public AnimalSQLOpenHelper(String name, String sound, String description) {
-        this.name = name;
-        this.sound = sound;
-        this.description = description;
+    public static final String CREATE_ANIMALS_TABLE = "CREATE TABLE " + TABLE_NAME +
+            "(" +
+            COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            COL_NAME + " TEXT, " +
+            COL_SOUND + " TEXT, " +
+            COL_DESCRIPTION + " TEXT)";
+
+
+    public AnimalSqlOpenHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
-    public String getName() {
-        return name;
+    public static AnimalSqlOpenHelper getInstance(Context context) {
+        if (curInstance == null) {
+            curInstance = new AnimalSqlOpenHelper(context.getApplicationContext());
+        }
+        return curInstance;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
 
-    public String getSound() {
-        return sound;
-    }
-
-    public void setSound(String sound) {
-        this.sound = sound;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_ANIMALS_TABLE);
     }
 
     @Override
-    public String toString() {
-        return name;
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        this.onCreate(db);
     }
+
+    public void addAnimal(Animal animal) {
+        ContentValues values = new ContentValues();
+        values.put(COL_NAME, animal.getName());
+        values.put(COL_SOUND, animal.getSound());
+        values.put(COL_DESCRIPTION, animal.getDescription());
+        SQLiteDatabase db = getWritableDatabase();
+        db.insert(TABLE_NAME, null, values);
+        db.close();
+    }
+
+    public void removeAnimal(int id) {
+        SQLiteDatabase db = getWritableDatabase();
+        db.delete(TABLE_NAME, COL_ID + "= ?", new String[]{String.valueOf(id)});
+        db.close();
+    }
+
+    public Cursor getAnimals() {
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor cursor = db.query(TABLE_NAME, ANIMAL_COLUMNS, null, null, null, null, null);
+        return cursor;
+    }
+
+}
+
+
 }
