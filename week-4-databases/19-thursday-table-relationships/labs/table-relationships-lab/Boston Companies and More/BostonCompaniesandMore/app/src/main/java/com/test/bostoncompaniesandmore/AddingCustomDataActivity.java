5526072697bcad4d4f7f5d0807bdbcc9a7bb026c
langsmith <langstonlmcs@gmail.com>
2016-06-30 10:41:09 -0700

    Add activities and layouts. Finished with adding new employee layout. About to begin on Main Activity layout

---
+++ week-4-databases/19-thursday-table-relationships/labs/table-relationships-lab/Boston Companies and More/BostonCompaniesandMore/app/src/main/java/com/test/bostoncompaniesandmore/AddingCustomDataActivity.java
@@ -0,0 +1,13 @@
+package com.test.bostoncompaniesandmore;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddingCustomDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adding_custom_data);
+    }
+}
