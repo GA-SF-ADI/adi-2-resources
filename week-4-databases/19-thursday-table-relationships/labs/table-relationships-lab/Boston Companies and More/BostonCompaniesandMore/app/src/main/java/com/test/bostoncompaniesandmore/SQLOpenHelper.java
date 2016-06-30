528f1cc83a1dfef96e25d885a774e9bfce776cf5
langsmith <langstonlmcs@gmail.com>
2016-06-30 12:05:24 -0700

    Finished both tables' static strings of individual columns and creating table

--- week-4-databases/19-thursday-table-relationships/labs/table-relationships-lab/Boston Companies and More/BostonCompaniesandMore/app/src/main/java/com/test/bostoncompaniesandmore/SQLOpenHelper.java
+++ week-4-databases/19-thursday-table-relationships/labs/table-relationships-lab/Boston Companies and More/BostonCompaniesandMore/app/src/main/java/com/test/bostoncompaniesandmore/SQLOpenHelper.java
@@ -8,24 +8,34 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class SQLOpenHelper extends SQLiteOpenHelper {
 
     private static String EMPLOYEE_TABLE_NAME = "employee";
-    private static String COL_ID = "_id";
-    private static String COL_SSN = "_id";
-    private static String COL_FIRST_NAME = "_id";
-    private static String COL_LAST_NAME = "_id";
-    private static String COL_YEAR_OF_BIRTH = "_id";
-    private static String COL_CITY = "_id";
+    private static String COL_SSN = "SSN";
+    private static String COL_FIRST_NAME = "First Name";
+    private static String COL_LAST_NAME = "Last Name";
+    private static String COL_YEAR_OF_BIRTH = "Year of Birth";
+    private static String COL_CITY = "City";
 
     private static String CREATE_TABLE = "CREATE TABLE " + EMPLOYEE_TABLE_NAME;
-
-
+    private static final String SQL_CREATE_EMPLOYEE_TABLE = "CREATE TABLE " +
+            EMPLOYEE_TABLE_NAME + " (" +
+            COL_SSN + " INTEGER PRIMARY KEY," +
+            COL_FIRST_NAME + " TEXT," +
+            COL_LAST_NAME + " TEXT," +
+            COL_YEAR_OF_BIRTH + " INTEGER," +
+            COL_CITY + " TEXT);";
 
 
     private static String JOB_TABLE_NAME = "job";
 
-    private static String COL_COMPANY = "_id";
-    private static String COL_SALARY = "_id";
+    private static String COL_COMPANY = "company";
+    private static String COL_SALARY = "salary";
+    private static String COL_EXPERIENCE = "experience";
 
-    private static String CREATE_NEW_TABLE = "CREATE TABLE ";
+    private static final String SQL_CREATE_JOB_TABLE = "CREATE TABLE " +
+            JOB_TABLE_NAME + " (" +
+            COL_SSN + " INTEGER PRIMARY KEY," +
+            COL_COMPANY + " TEXT," +
+            COL_SALARY + " INTEGER," +
+            COL_EXPERIENCE + " INTEGER " + ");";
 
 
 }
