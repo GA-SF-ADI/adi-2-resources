075d5e485ec63847cb9882bb521d88e9cb558f8f
langsmith <langstonlmcs@gmail.com>
2016-06-30 11:53:04 -0700

    Filling out private static strings in OpenHelper file

--- week-4-databases/19-thursday-table-relationships/labs/table-relationships-lab/Boston Companies and More/BostonCompaniesandMore/app/src/main/java/com/test/bostoncompaniesandmore/AddingCustomDataActivity.java
+++ week-4-databases/19-thursday-table-relationships/labs/table-relationships-lab/Boston Companies and More/BostonCompaniesandMore/app/src/main/java/com/test/bostoncompaniesandmore/AddingCustomDataActivity.java
@@ -3,6 +3,8 @@ package com.test.bostoncompaniesandmore;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class AddingCustomDataActivity extends AppCompatActivity {
@@ -17,9 +19,36 @@ public class AddingCustomDataActivity extends AppCompatActivity {
         EditText lastNameText = (EditText) findViewById(R.id.editText_last_name);
         EditText yearOfBirthText = (EditText) findViewById(R.id.editText_year_of_birth);
         EditText cityText = (EditText) findViewById(R.id.editText_city);
+        Button addCustomData = (Button) findViewById(R.id.button_add_custom_data_button);
+        Button cancelAndReturnToMainActivity = (Button) findViewById(R.id.button_cancel_and_return_to_main_activity);
 
         Intent receivedIntent = getIntent();
 
+        addCustomData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+        });
+
+        cancelAndReturnToMainActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+
+            }
+        });
 
     }
 }
--- week-4-databases/19-thursday-table-relationships/labs/table-relationships-lab/Boston Companies and More/BostonCompaniesandMore/app/src/main/java/com/test/bostoncompaniesandmore/SQLOpenHelper.java
+++ week-4-databases/19-thursday-table-relationships/labs/table-relationships-lab/Boston Companies and More/BostonCompaniesandMore/app/src/main/java/com/test/bostoncompaniesandmore/SQLOpenHelper.java
@@ -7,7 +7,25 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 public class SQLOpenHelper extends SQLiteOpenHelper {
 
-    private static String
+    private static String EMPLOYEE_TABLE_NAME = "employee";
+    private static String COL_ID = "_id";
+    private static String COL_SSN = "_id";
+    private static String COL_FIRST_NAME = "_id";
+    private static String COL_LAST_NAME = "_id";
+    private static String COL_YEAR_OF_BIRTH = "_id";
+    private static String COL_CITY = "_id";
+
+    private static String CREATE_TABLE = "CREATE TABLE " + EMPLOYEE_TABLE_NAME;
+
+
+
+
+    private static String JOB_TABLE_NAME = "job";
+
+    private static String COL_COMPANY = "_id";
+    private static String COL_SALARY = "_id";
+
+    private static String CREATE_NEW_TABLE = "CREATE TABLE ";
 
 
 }
