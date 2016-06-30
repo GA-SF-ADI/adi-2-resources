378424ac52b10994544d440edf60c7ad19303b58
Aleksandr Tomak <alex@Aleksandrs-MacBook-Pro.local>
2016-06-30 12:24:04 -0700

    Update gradle independet work

--- week-4-databases/20-friday-enable-search/lessons/enable-search-lesson/starter-code/SearchViewDemoGuidedPractice/SearchViewDemo/app/src/main/java/ly/generalassemb/drewmahrt/searchviewdemo/NumbersSQLiteHelper.java
+++ week-4-databases/20-friday-enable-search/lessons/enable-search-lesson/starter-code/SearchViewDemoGuidedPractice/SearchViewDemo/app/src/main/java/ly/generalassemb/drewmahrt/searchviewdemo/NumbersSQLiteHelper.java
@@ -1,7 +1,6 @@
 package ly.generalassemb.drewmahrt.searchviewdemo;
 
 import android.content.Context;
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
