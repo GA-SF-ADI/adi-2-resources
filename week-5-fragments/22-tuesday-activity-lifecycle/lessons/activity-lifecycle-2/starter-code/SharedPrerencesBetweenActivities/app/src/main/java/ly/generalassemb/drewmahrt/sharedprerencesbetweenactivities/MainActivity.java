32f9f9533cee099e7c4ee4c7b065e984ae6448ea
sterlingann <sterlingann@gmail.com>
2016-07-05 16:05:55 -0700

    added crow foot diagram

--- week-5-fragments/22-tuesday-activity-lifecycle/lessons/activity-lifecycle-2/starter-code/SharedPrerencesBetweenActivities/app/src/main/java/ly/generalassemb/drewmahrt/sharedprerencesbetweenactivities/MainActivity.java
+++ week-5-fragments/22-tuesday-activity-lifecycle/lessons/activity-lifecycle-2/starter-code/SharedPrerencesBetweenActivities/app/src/main/java/ly/generalassemb/drewmahrt/sharedprerencesbetweenactivities/MainActivity.java
@@ -1,13 +1,14 @@
 package ly.generalassemb.drewmahrt.sharedprerencesbetweenactivities;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.content.SharedPreferences.Editor;
 
 public class MainActivity extends AppCompatActivity {
     private EditText editText;
@@ -26,14 +27,16 @@ public class MainActivity extends AppCompatActivity {
                 SharedPreferences sterlingPreference = getPreferences(Context.MODE_PRIVATE);
 
                 //Shared preferences "reads" the string
-                String str = sterlingPreference.getString("myKey","DEFAULT");
+                String str = sterlingPreference.getString("myKey", "DEFAULT");
 
                 //to save my Sterling Preference I must retrieve an object called editor
                 //to do this i have to remember to import api android:content.sharedpreferences.editor
                 Editor editor = sterlingPreference.edit();
-                editor.putString("myKey",str+" World!");
+                editor.putString("myKey", str + " World!");
                 editor.commit();
+                Intent i = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(i);
             }
         });
-    }
+
 }
