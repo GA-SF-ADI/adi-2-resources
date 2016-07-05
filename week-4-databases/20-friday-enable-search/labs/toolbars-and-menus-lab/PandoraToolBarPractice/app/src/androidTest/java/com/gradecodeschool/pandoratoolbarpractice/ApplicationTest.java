efa86130e311f1935d70c1ed3e6258d8a570ac9a
sterlingann <sterlingann@gmail.com>
2016-07-04 17:24:00 -0700

    toolbar and menu lab

---
+++ week-4-databases/20-friday-enable-search/labs/toolbars-and-menus-lab/PandoraToolBarPractice/app/src/androidTest/java/com/gradecodeschool/pandoratoolbarpractice/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.gradecodeschool.pandoratoolbarpractice;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
