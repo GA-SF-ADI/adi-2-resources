04ce067f16ceb18162644261d0e955f2fd920f51
HollisDayCheer <hollisj@carleton.edu>
2016-07-05 16:01:42 -0700

    updated starter code

--- week-5-fragments/22-tuesday-activity-lifecycle/labs/activity-lifecycle-shared-preferences/starter-code/SharedPreferencesExample/app/src/main/java/com/example/hollis/sharedpreferenceexample/WeatherSQliteOpenHelper.java
+++ week-5-fragments/22-tuesday-activity-lifecycle/labs/activity-lifecycle-shared-preferences/starter-code/SharedPreferencesExample/app/src/main/java/com/example/hollis/sharedpreferenceexample/WeatherSQliteOpenHelper.java
@@ -67,18 +67,7 @@ public class WeatherSQliteOpenHelper extends SQLiteOpenHelper {
     }
 
     //This takes in a column name and gets
-    public Cursor getTemp(String wantedTemp){
-        String column;
-        switch(wantedTemp){
-            case "celsius":
-                column = COL_TEMP_CEL;
-                break;
-            case "kelvin":
-                column = COL_TEMP_KELVIN;
-                break;
-            default:
-                column = COL_TEMP_FAHR;
-        }
+    public Cursor getTemp(String column){
         SQLiteDatabase db = getReadableDatabase();
         String[] projection = {COL_ID, COL_DAY, column};
         Cursor cursor = db.query(TABLE_NAME,projection, null, null, null, null, null);
