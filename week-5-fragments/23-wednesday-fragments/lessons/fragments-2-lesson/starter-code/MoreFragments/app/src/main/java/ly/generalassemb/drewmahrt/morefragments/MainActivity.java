4594f6e5544cdccad7b0a0deee619ee18819e4d3
Aleksandr Tomak <alex@Aleksandrs-MacBook-Pro.local>
2016-07-05 14:57:56 -0700

    Create starter code for independent practice

---
+++ week-5-fragments/23-wednesday-fragments/lessons/fragments-2-lesson/starter-code/MoreFragments/app/src/main/java/ly/generalassemb/drewmahrt/morefragments/MainActivity.java
@@ -0,0 +1,20 @@
+package ly.generalassemb.drewmahrt.morefragments;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity { // TODO: 1. implement your Interface
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    // TODO: 2. Implement the interface method
+    // TODO: 2a. Grab DetailFragment from FragmentManager
+    // TODO: 2b. Use your DetailFragment's instance and pass selectedPlanet to the helper method
+
+
+    
+}
---
+++ week-5-fragments/23-wednesday-fragments/lessons/fragments-2-lesson/starter-code/MoreFragments/app/src/main/java/ly/generalassemb/drewmahrt/morefragments/MyListFragment.java
@@ -0,0 +1,37 @@
+package ly.generalassemb.drewmahrt.morefragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+/**
+ * Created by drewmahrt on 2/25/16.
+ */
+public class MyListFragment extends ListFragment {
+    // TODO: 1. create a OnPlanetSelectedListener.java interface in its own file
+
+    // TODO: 2. declare a OnPlanetSelectedListener variable
+
+
+    // TODO: 3. Override onAttach()
+    // TODO: 3a. inside onAttach() set the value of your variable you created in 2.
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(),android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.Planets));
+        setListAdapter(adapter);
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        // TODO: 4. Pass the selected planet to the interface variable
+    }
+}
