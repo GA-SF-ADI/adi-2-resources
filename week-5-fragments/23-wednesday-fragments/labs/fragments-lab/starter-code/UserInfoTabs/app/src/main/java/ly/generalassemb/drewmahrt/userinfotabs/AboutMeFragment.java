9296ee1c740f59350dde7a91b8786a7347a40da2
Nolberto Arroyo <nolberto.arroyo@gmail.com>
2016-07-06 12:51:57 -0700

    UserInfoTabs lab

---
+++ week-5-fragments/23-wednesday-fragments/labs/fragments-lab/starter-code/UserInfoTabs/app/src/main/java/ly/generalassemb/drewmahrt/userinfotabs/AboutMeFragment.java
@@ -0,0 +1,18 @@
+package ly.generalassemb.drewmahrt.userinfotabs;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by nolbertoarroyo on 7/6/16.
+ */
+public class AboutMeFragment extends android.support.v4.app.Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.about_me_fragment,container,false);
+    }
+}
