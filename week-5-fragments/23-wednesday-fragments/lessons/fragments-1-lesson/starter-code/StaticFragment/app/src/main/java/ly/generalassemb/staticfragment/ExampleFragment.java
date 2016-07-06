b46a795d94dd7dd4824412407033a0df482c14c3
Nolberto Arroyo <nolberto.arroyo@gmail.com>
2016-07-06 12:02:05 -0700

    weather fragments IP

---
+++ week-5-fragments/23-wednesday-fragments/lessons/fragments-1-lesson/starter-code/StaticFragment/app/src/main/java/ly/generalassemb/staticfragment/ExampleFragment.java
@@ -0,0 +1,20 @@
+package ly.generalassemb.staticfragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by nolbertoarroyo on 7/6/16.
+ */
+public class ExampleFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_example,container,false);
+    }
+}
