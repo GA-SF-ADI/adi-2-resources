dec354265334893194a6cbb335ab3ee1c1a06996
sterlingann <sterlingann@gmail.com>
2016-07-06 11:15:08 -0700

    static fragment exercise

---
+++ week-5-fragments/23-wednesday-fragments/lessons/fragments-1-lesson/starter-code/StaticFragment/app/src/main/java/ly/generalassemb/staticfragment/Fragment.java
@@ -0,0 +1,19 @@
+package ly.generalassemb.staticfragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by sterlinggerritz on 7/6/16.
+ */
+public class Fragment extends android.app.Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_example, container, false);
+    }
+}
+
