8a3ce3ea6dc0b6a46bf46f6020d520f4057f8c48
Nolberto Arroyo <nolberto.arroyo@gmail.com>
2016-07-06 06:45:07 -0700

    sharedprefs lab and hw

--- week-5-fragments/22-tuesday-activity-lifecycle/labs/activity-lifecycle-shared-preferences/starter-code/SharedPreferencesExample/app/src/main/java/com/example/hollis/sharedpreferenceexample/MainActivity.java
+++ week-5-fragments/22-tuesday-activity-lifecycle/labs/activity-lifecycle-shared-preferences/starter-code/SharedPreferencesExample/app/src/main/java/com/example/hollis/sharedpreferenceexample/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.hollis.sharedpreferenceexample;
 
+import android.content.SharedPreferences;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,6 +17,11 @@ public class MainActivity extends AppCompatActivity {
     Button fahr;
     Button cels;
     Button kelv;
+    SharedPreferences sharedPreferences;
+    String lastTemp;
+    public static final String TEMP = "temperature measurement";
+    public static final String DATA_INSERTED = "DATA INSERTED";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,16 +33,26 @@ public class MainActivity extends AppCompatActivity {
 
         //inserts weathers into our database
         //TODO: THIS WILL RUN ON ONCREATE EVERY TIME, USE SHAREDPREFERENCES TO MAKE IT NOT HAPPEN
-        Weather weather1 = new Weather("Monday", 20, 78, 295);
-        Weather weather2 = new Weather("Tuesday", 4, 23, 232);
-        Weather weather3 = new Weather("Wednesday", 2, 42, 123);
-        Weather weather4 = new Weather("Thursday", 13, 21, 245);
-        Weather weather5 = new Weather("Friday", 18, 32, 231);
-        helper.insert(weather1);
-        helper.insert(weather2);
-        helper.insert(weather3);
-        helper.insert(weather4);
-        helper.insert(weather5);
+        sharedPreferences= getSharedPreferences("main",MODE_PRIVATE);
+
+        boolean hasInserted =sharedPreferences.getBoolean(DATA_INSERTED, false);
+        if(!hasInserted) {
+
+
+            Weather weather1 = new Weather("Monday", 20, 78, 295);
+            Weather weather2 = new Weather("Tuesday", 4, 23, 232);
+            Weather weather3 = new Weather("Wednesday", 2, 42, 123);
+            Weather weather4 = new Weather("Thursday", 13, 21, 245);
+            Weather weather5 = new Weather("Friday", 18, 32, 231);
+            helper.insert(weather1);
+            helper.insert(weather2);
+            helper.insert(weather3);
+            helper.insert(weather4);
+            helper.insert(weather5);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean(DATA_INSERTED, true);
+            editor.commit();
+        }
 
 
         listView = (ListView) findViewById(R.id.listview);
@@ -44,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
         //These Click Listeners determine which type of temperature should be shown
         //TODO: Save the users preferences on shared preferences
         //TODO: Have the correct one load inside of onResume;
+         lastTemp = sharedPreferences.getString(TEMP,"DEFAULT");
         fahr = (Button) findViewById(R.id.fahr_button);
         cels = (Button) findViewById(R.id.celsius_button);
         kelv = (Button) findViewById(R.id.kelvin_button);
@@ -51,6 +68,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 setAdapter(WeatherSQliteOpenHelper.COL_TEMP_FAHR);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString(TEMP, WeatherSQliteOpenHelper.COL_TEMP_FAHR);
+                editor.commit();
             }
         });
 
@@ -58,12 +78,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 setAdapter(WeatherSQliteOpenHelper.COL_TEMP_CEL);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString(TEMP, WeatherSQliteOpenHelper.COL_TEMP_CEL);
+                editor.commit();
             }
         });
         kelv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 setAdapter(WeatherSQliteOpenHelper.COL_TEMP_KELVIN);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString(TEMP, WeatherSQliteOpenHelper.COL_TEMP_KELVIN);
+                editor.commit();
             }
         });
     }
@@ -79,8 +105,22 @@ public class MainActivity extends AppCompatActivity {
                     0);
             listView.setAdapter(adapter);
         }else{
-            Toast.makeText(this, "Click a Button to show tempuratures!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Click a Button to show temperatures!", Toast.LENGTH_SHORT).show();
         }
     }
 
+    @Override
+    protected void onResume() {
+        setAdapter(lastTemp);
+        //on resume setting the view to lastTemp
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+
+        super.onPause();
+    }
 }
