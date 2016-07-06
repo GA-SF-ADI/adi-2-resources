54827d66a339a65ccc0a81c78fc072fb2aa701ce
Aleksandr Tomak <alex@Aleksandrs-MacBook-Pro.local>
2016-07-05 19:08:46 -0700

    Create starter code

--- week-5-fragments/23-wednesday-fragments/lessons/fragments-1-lesson/starter-code/DynamicFragment/app/src/main/java/ly/generalassemb/dynamicfragment/MainActivity.java
+++ week-5-fragments/23-wednesday-fragments/lessons/fragments-1-lesson/starter-code/DynamicFragment/app/src/main/java/ly/generalassemb/dynamicfragment/MainActivity.java
@@ -1,9 +1,6 @@
 package ly.generalassemb.dynamicfragment;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -18,16 +15,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         setupButtons();
 
-        // setup fragment manager and transaction
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        // TODO: setup fragment manager and transaction
 
-        // create dailyForecast fragment
-        DailyForecast dailyForecast = new DailyForecast();
+        // TODO: create dailyForecast fragment
 
-        // add the daily forecast fragment to the container and commit the change
-        fragmentTransaction.add(R.id.weather_fragment_container, dailyForecast);
-        fragmentTransaction.commit();
+        // TODO: add the daily forecast fragment to the container and commit the change
+
     }
 
     private void setupButtons(){
@@ -40,27 +33,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
-        // create fragment for code re-use. We don't want to repeat code.
-        Fragment fragment = null;
         switch (v.getId()){
             case R.id.daily_button:
-                fragment = new DailyForecast();
+                // todo replace with dailyWeather fragment
                 break;
             case R.id.weekly_button:
-                fragment = new WeekForecast();
+                // todo replace with weeklyWeather fragment
                 break;
             default:
                 break;
         }
 
-        // only need to replace and commit if the fragment was created
-        if (fragment != null){
-            fragmentTransaction.replace(R.id.weather_fragment_container, fragment);
-            fragmentTransaction.commit();
-        }
     }
 
 }
