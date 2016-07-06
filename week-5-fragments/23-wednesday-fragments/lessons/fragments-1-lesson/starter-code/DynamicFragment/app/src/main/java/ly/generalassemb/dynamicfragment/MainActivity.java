2e9dfb98032656fc767dd20a6c0739fd09e76713
Audrey So <audreyeso@Audreys-MacBook-Air.local>
2016-07-06 12:08:41 -0700

    Dynamic Fragment Lesson

--- week-5-fragments/23-wednesday-fragments/lessons/fragments-1-lesson/starter-code/DynamicFragment/app/src/main/java/ly/generalassemb/dynamicfragment/MainActivity.java
+++ week-5-fragments/23-wednesday-fragments/lessons/fragments-1-lesson/starter-code/DynamicFragment/app/src/main/java/ly/generalassemb/dynamicfragment/MainActivity.java
@@ -1,12 +1,17 @@
 package ly.generalassemb.dynamicfragment;
 
+import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button dailyWeather, weeklyWeather;
+    FragmentDaily dailyForecastFragment;
+    FragmentWeek weeklyFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +21,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setupButtons();
 
         // TODO: setup fragment manager and transaction
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
         // TODO: create dailyForecast fragment
+        dailyForecastFragment = new FragmentDaily();
+        weeklyFragment = new FragmentWeek();
 
         // TODO: add the daily forecast fragment to the container and commit the change
+        fragmentTransaction.add(R.id.fragment_dynamic_container, dailyForecastFragment);
+        fragmentTransaction.commit();
+
 
     }
 
-    private void setupButtons(){
+    private void setupButtons() {
         dailyWeather = (Button) findViewById(R.id.daily_button);
         weeklyWeather = (Button) findViewById(R.id.weekly_button);
 
@@ -34,12 +46,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
 
-        switch (v.getId()){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        switch (v.getId()) {
             case R.id.daily_button:
                 // todo replace with dailyWeather fragment
+
+                fragmentTransaction.replace(R.id.fragment_dynamic_container,dailyForecastFragment);
+                fragmentTransaction.commit();
                 break;
             case R.id.weekly_button:
                 // todo replace with weeklyWeather fragment
+
+                fragmentTransaction.replace(R.id.fragment_dynamic_container,weeklyFragment);
+                fragmentTransaction.commit();
                 break;
             default:
                 break;
