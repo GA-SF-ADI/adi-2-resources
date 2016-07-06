619a0e44f86d2c073ff8cfcd8ce9a779626e7e92
Patrick Cummins <patrickcummins2009@gmail.com>
2016-07-06 12:00:11 -0700

    Basic Req 's finished

--- week-5-fragments/23-wednesday-fragments/labs/fragments-lab/starter-code/UserInfoTabs/app/src/main/java/ly/generalassemb/drewmahrt/userinfotabs/PagerAdapter.java
+++ week-5-fragments/23-wednesday-fragments/labs/fragments-lab/starter-code/UserInfoTabs/app/src/main/java/ly/generalassemb/drewmahrt/userinfotabs/PagerAdapter.java
@@ -25,8 +25,11 @@ public class PagerAdapter extends FragmentStatePagerAdapter {
                 return tab1;
             case 1:
                 //TODO: Add your second tab
+                AboutFragment tab2 = new AboutFragment();
+                return tab2;
             case 2:
                 //TODO: Add your third tab
+                ContactFragment tab3 = new ContactFragment();
                 return tab3;
             default:
                 return null;
---
+++ week-5-fragments/23-wednesday-fragments/labs/fragments-lab/starter-code/UserInfoTabs/app/src/main/java/ly/generalassemb/drewmahrt/userinfotabs/PictureFragment.java
@@ -0,0 +1,20 @@
+package ly.generalassemb.drewmahrt.userinfotabs;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by patrickcummins on 7/6/16.
+ */
+public class PictureFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_picture, container, false);
+    }
+}
