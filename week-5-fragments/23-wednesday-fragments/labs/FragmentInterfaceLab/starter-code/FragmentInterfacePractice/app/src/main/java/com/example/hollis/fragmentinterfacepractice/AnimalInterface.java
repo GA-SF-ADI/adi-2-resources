1c03992659b6ce3f3ae847807e1b7bc591bcb718
Michael Jeffress <michaeljeffress@Michaels-MacBook-Pro.local>
2016-07-07 08:55:22 -0700

    Wednesday lab

---
+++ week-5-fragments/23-wednesday-fragments/labs/FragmentInterfaceLab/starter-code/FragmentInterfacePractice/app/src/main/java/com/example/hollis/fragmentinterfacepractice/AnimalInterface.java
@@ -0,0 +1,9 @@
+package com.example.hollis.fragmentinterfacepractice;
+
+/**
+ * Created by michaeljeffress on 7/6/16.
+ */
+public interface AnimalInterface {
+    public void animalListener(Animal animal);
+}
+
