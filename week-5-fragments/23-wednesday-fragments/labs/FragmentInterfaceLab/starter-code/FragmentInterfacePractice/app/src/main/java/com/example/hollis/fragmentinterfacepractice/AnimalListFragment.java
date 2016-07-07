7a640c903eb4849211f589bcc21c192a185d6fca
Nolberto Arroyo <nolberto.arroyo@gmail.com>
2016-07-07 09:03:24 -0700

    afternoon lab, problem with loading second fragment

---
+++ week-5-fragments/23-wednesday-fragments/labs/FragmentInterfaceLab/starter-code/FragmentInterfacePractice/app/src/main/java/com/example/hollis/fragmentinterfacepractice/AnimalListFragment.java
@@ -0,0 +1,49 @@
+package com.example.hollis.fragmentinterfacepractice;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
+
+/**
+ * Created by nolbertoarroyo on 7/6/16.
+ */
+public class AnimalListFragment extends android.support.v4.app.ListFragment{
+    List<Animal> animalList;
+    OnAnimalSelectedListener onAnimalSelectedListener;
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        onAnimalSelectedListener = (OnAnimalSelectedListener)getActivity();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        ArrayAdapter<Animal> adapter = new ArrayAdapter<Animal>(container.getContext(),android.R.layout.simple_list_item_1,animalList);
+        setListAdapter(adapter);
+        return super.onCreateView(inflater, container, savedInstanceState);
+
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        Animal selectedAnimal = animalList.get(position);
+        onAnimalSelectedListener.onAnimalSelected(selectedAnimal);
+    }
+
+    public void setAnimalList(List<Animal> animalList) {
+        this.animalList = animalList;
+    }
+}
