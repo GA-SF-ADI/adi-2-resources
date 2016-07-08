5a2ffc98d3c4188f9c3dc5bc5bb92840904449bc
Michael Jeffress <michaeljeffress@Michaels-MacBook-Pro.local>
2016-07-08 08:49:59 -0700

    Thursday work

--- week-5-fragments/24-thursday-multiple-devices/labs/usability-testing/starter-code/UsabilityTestingStarterCode/app/src/main/java/generalassembly/yuliyakaleda/usabilitytestingstartercode/MainActivity.java
+++ week-5-fragments/24-thursday-multiple-devices/labs/usability-testing/starter-code/UsabilityTestingStarterCode/app/src/main/java/generalassembly/yuliyakaleda/usabilitytestingstartercode/MainActivity.java
@@ -2,7 +2,7 @@ package generalassembly.yuliyakaleda.usabilitytestingstartercode;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
 
     //TODO: Create a separate layout xml file for tablet devices.
+
+        View view = findViewById(R.id.web_view);
+          if (view == null){
+            //this is a phone
+              view
+          }
+          else {
+            //this is a tablet
+          }
   }
 }
