7fed2a5c08c1ce145e2c4ddfd3c5f701fce78fdf
langsmith <langstonlmcs@gmail.com>
2016-07-09 00:54:50 -0700

    Splash screen completely done

---
+++ projects/project-02/Snug/app/src/main/java/com/test/snug/SQLiteOpenHelper.java
@@ -0,0 +1,11 @@
+/*
+package com.test.snug;
+
+*/
+/**
+ * Created by LangstonSmith on 7/9/16.
+ *//*
+
+public class SQLiteOpenHelper extends android.database.sqlite.SQLiteOpenHelper {
+}
+*/
--- projects/project-02/Snug/app/src/main/java/com/test/snug/ShoppingCartActivity.java
+++ projects/project-02/Snug/app/src/main/java/com/test/snug/ShoppingCartActivity.java
@@ -3,11 +3,13 @@ package com.test.snug;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class ShoppingCartActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +18,13 @@ public class ShoppingCartActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+        final FloatingActionButton makeFinalPaymentFab = (FloatingActionButton) findViewById(R.id.fab_make_final_payment);
+        makeFinalPaymentFab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+
             }
         });
     }
