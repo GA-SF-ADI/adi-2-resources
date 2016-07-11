1ae862a143cdc099ee870664e1ca47d0ff46bad2
jon k <jonkk.88@gmail.com>
2016-07-11 14:00:25 -0700

    started adding db

---
+++ projects/project-02/MyApplication/app/src/main/java/com/test/myapplication/CustomAlcAdapter.java
@@ -0,0 +1,41 @@
+package com.test.myapplication;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by Jon Kim on 7/11/16.
+ */
+public class CustomAlcAdapter extends CursorAdapter{
+    TextView listAlcName, listAlcPrice;
+    ImageView listAlcImage;
+
+
+    public CustomAlcAdapter(Context context, Cursor c, int flags) {
+        super(context, c, flags);
+
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        listAlcName = (TextView)view.findViewById(R.id.list_alc_name);
+        listAlcImage = (ImageView)view.findViewById(R.id.list_alc_image);
+        listAlcPrice = (TextView)view.findViewById(R.id.list_alc_price);
+
+        listAlcName.setText(cursor.getString(cursor.getColumnIndex(DataBaseHelper.COL_ALC_NAME)));
+        listAlcPrice.setText(cursor.getInt(cursor.getColumnIndex(DataBaseHelper.COL_ALC_PRICE)));
+        listAlcImage.setImageResource(cursor.getInt(cursor.getColumnIndex(DataBaseHelper.COL_ALC_IMAGE)));
+
+    }
+}
--- projects/project-02/MyApplication/app/src/main/java/com/test/myapplication/MainActivity.java
+++ projects/project-02/MyApplication/app/src/main/java/com/test/myapplication/MainActivity.java
@@ -4,13 +4,18 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    ListView mainListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainListView = (ListView) findViewById(R.id.main_list_view);
+
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -18,5 +23,13 @@ public class MainActivity extends AppCompatActivity {
         inflater.inflate(R.menu.alc_menu, menu);
 
         return true;
+    }
+
+    public void addData(){
+        DataBaseHelper db = new DataBaseHelper(this);
+        Alcohol jameson = new Alcohol("Jameson", 23, 40, "Blended Irish whiskey",1,1);
+        Alcohol greygoose = new Alcohol("Grey Goose", )
+
+
     }
 }
