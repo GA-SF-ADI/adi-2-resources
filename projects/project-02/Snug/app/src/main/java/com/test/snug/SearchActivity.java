1ba6b18c45e8cfe848e0cb7a143792f02cac5b86
langsmith <langstonlmcs@gmail.com>
2016-07-12 12:38:46 -0700

    Search layout is done!

--- projects/project-02/Snug/app/src/main/java/com/test/snug/SearchActivity.java
+++ projects/project-02/Snug/app/src/main/java/com/test/snug/SearchActivity.java
@@ -6,6 +6,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -16,17 +18,70 @@ public class SearchActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.main_activity_main_toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        final FloatingActionButton startSearchFAB = (FloatingActionButton) findViewById(R.id.fab_to_start_search);
+
+        final RadioButton fittedRadioButton = (RadioButton) findViewById(R.id.fitted_cap_radio_button);
+        final RadioButton snapbackRadioButton = (RadioButton) findViewById(R.id.snapback_cap_radio_button);
+        final boolean fitted = false;
+        final boolean snapback = false;
+
+
+        fittedRadioButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+
+                if (fittedRadioButton.isChecked()) {
+                    if (!fitted) {
+                        fittedRadioButton.setChecked(true);
+                        snapbackRadioButton.setChecked(false);
+
+                    } else {
+
+                        fittedRadioButton.setChecked(false);
+                        snapbackRadioButton.setChecked(false);
+                    }
+                }
+
+            }
+
+
+        });
+
+        snapbackRadioButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (snapbackRadioButton.isChecked()) {
+                    if (!snapback) {
+                        snapbackRadioButton.setChecked(true);
+                        fittedRadioButton.setChecked(false);
+
+                    } else {
+
+                        snapbackRadioButton.setChecked(false);
+                        fittedRadioButton.setChecked(false);
+                    }
+                }
+
+
+            }
+        });
+
+        startSearchFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
                 Intent intent = new Intent(SearchActivity.this, MainActivity.class);
                 startActivity(intent);
 
 
             }
         });
+
+
     }
+
+
+
 
 }
