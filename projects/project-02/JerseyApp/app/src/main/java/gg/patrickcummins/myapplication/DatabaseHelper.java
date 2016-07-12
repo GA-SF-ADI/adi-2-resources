384fcc98c9ab6cbd3ace2be78dd86b1410316332
Finnegan1441 <patrickcummins2009@gmail.com>
2016-07-12 15:10:49 -0700

    added Purchase History db and Layouts

--- projects/project-02/JerseyApp/app/src/main/java/gg/patrickcummins/myapplication/DatabaseHelper.java
+++ projects/project-02/JerseyApp/app/src/main/java/gg/patrickcummins/myapplication/DatabaseHelper.java
@@ -9,18 +9,22 @@ import android.provider.BaseColumns;
 
 import java.util.ArrayList;
 
+import static android.R.attr.max;
+
 /**
  * Created by patrickcummins on 7/9/16.
  */
-//TODO:Implement Cart Table
+
 public class DatabaseHelper extends SQLiteOpenHelper {
     private static DatabaseHelper INSTANCE;
 
     private static final String CREATE_PLAYER_TABLE = "CREATE TABLE " + PlayerValues.TABLE_NAME + " (" + PlayerValues._ID + " INTEGER PRIMARY KEY, " + PlayerValues.NAME + " TEXT, " + PlayerValues.TEAM + " TEXT, " + PlayerValues.NUMBER + " INTEGER, " + PlayerValues.POSITION + " TEXT, " + PlayerValues.PICTURE + " INTEGER, " + PlayerValues.JERSEY1 + " INTEGER, " + PlayerValues.JERSEY2 + " INTEGER, " + PlayerValues.JERSEY3 + " INTEGER)";
     private static final String CREATE_CART_TABLE = "CREATE TABLE " + CartValues.TABLE_NAME + " (" + CartValues._ID + " INTEGER PRIMARY KEY, " + CartValues.PLAYERNAME + " TEXT, " + CartValues.COLOR + " TEXT, " + CartValues.PICTURE + " INTEGER, " + CartValues.PRICE + " REAL)";
+    private static final String CREATE_PURCHASE_HISTORY_TABLE= "CREATE TABLE " + PurchaseHistoryValues.TABLE_NAME + " (" + PurchaseHistoryValues._ID + " INTEGER PRIMARY KEY, " + PurchaseHistoryValues.HISTORY_ID + " INTEGER, " + PurchaseHistoryValues.PLAYERNAME + " TEXT, " + PurchaseHistoryValues.COLOR + " TEXT, " + PurchaseHistoryValues.DATE + " TEXT, " + PurchaseHistoryValues.PICTURE + " INTEGER, " + PurchaseHistoryValues.PRICE + " REAL)";
 
     private static final String DROP_PLAYER_TABLE = "DROP TABLE IF EXISTS " + PlayerValues.TABLE_NAME;
     private static final String DROP_CART_TABLE = "DROP TABLE IF EXISTS " + CartValues.TABLE_NAME;
+    private static final String DROP_PURCHASE_HISTORY_TABLE = "DROP TABLE IF EXISTS " + PurchaseHistoryValues.TABLE_NAME;
 
     public static abstract class PlayerValues implements BaseColumns {
         public static final String TABLE_NAME = "PlayerTable";
@@ -42,6 +46,16 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         public static final String COLOR = "Color";
 
     }
+    public static abstract class PurchaseHistoryValues implements BaseColumns{
+        public static final String TABLE_NAME = "HistoryTable";
+        public static final String HISTORY_ID = "HistoryID";
+        public static final String PICTURE = "JerseyPicture";
+        public static final String PLAYERNAME = "PlayerName";
+        public static final String PRICE = "Price";
+        public static final String COLOR = "Color";
+        public static final String DATE= "Date";
+
+    }
 
     private DatabaseHelper(Context context) {
         super(context, "db", null, 1);
@@ -59,6 +73,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(CREATE_PLAYER_TABLE);
         db.execSQL(CREATE_CART_TABLE);
+        db.execSQL(CREATE_PURCHASE_HISTORY_TABLE);
 
 
     }
@@ -67,6 +82,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL(DROP_PLAYER_TABLE);
         db.execSQL(DROP_CART_TABLE);
+        db.execSQL(DROP_PURCHASE_HISTORY_TABLE);
         onCreate(db);
     }
 
@@ -106,6 +122,18 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         db.insertOrThrow(CartValues.TABLE_NAME, null, values);
         db.close();
     }
+
+    public void insertPurchaseHistoryRow(CartItem cartItem, int currentOrder, String date){
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(PurchaseHistoryValues.HISTORY_ID, currentOrder);
+        values.put(PurchaseHistoryValues.PLAYERNAME, cartItem.getPlayerName());
+        values.put(PurchaseHistoryValues.COLOR, cartItem.getColor());
+        values.put(PurchaseHistoryValues.PICTURE, cartItem.getPicture());
+        values.put(PurchaseHistoryValues.PRICE, cartItem.getPrice());
+        values.put(PurchaseHistoryValues.DATE, date);
+    }
+
 
     public ArrayList<Player> getTeamPlayerList(String team) {
         ArrayList<Player> playerArrayList = new ArrayList<>();
@@ -153,6 +181,48 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         }
         cursor.close();
         return cartItemArrayList;
+    }
+    public ArrayList<CartItem> getCartItemArrayListByHistoryID(int historyID){
+        ArrayList<CartItem> cartItemArrayList = new ArrayList<>();
+        SQLiteDatabase db = getReadableDatabase();
+        String query = "SELECT " + PurchaseHistoryValues.PLAYERNAME + ", " + PurchaseHistoryValues.COLOR + ", " + PurchaseHistoryValues.PRICE + ", " + PurchaseHistoryValues.PICTURE + " FROM " + PurchaseHistoryValues.TABLE_NAME + " WHERE " + PurchaseHistoryValues.HISTORY_ID + " = " + historyID;
+        Cursor cursor = db.rawQuery(query, null);
+        while (cursor.moveToNext()) {
+            CartItem cartItem = new CartItem(cursor.getString(cursor.getColumnIndex(PurchaseHistoryValues.PLAYERNAME)), cursor.getString(cursor.getColumnIndex(PurchaseHistoryValues.COLOR)), cursor.getInt(cursor.getColumnIndex(PurchaseHistoryValues.PICTURE)), cursor.getDouble(cursor.getColumnIndex(PurchaseHistoryValues.PRICE)));
+            cartItemArrayList.add(cartItem);
+        }
+        cursor.close();
+        return cartItemArrayList;
+    }
+    public int getLastHistoryID(){
+        int max=0;
+        SQLiteDatabase db = getReadableDatabase();
+        String query = "SELECT MAX(" +PurchaseHistoryValues.HISTORY_ID+") FROM " + PurchaseHistoryValues.TABLE_NAME;
+        Cursor cursor = db.rawQuery(query, null);
+        cursor.moveToFirst();
+        max = cursor.getInt(cursor.getColumnIndex(PurchaseHistoryValues.HISTORY_ID));
+        cursor.close();
+        return max;
+
+    }
+    public double getPriceFromHistoryID(int historyID){
+        SQLiteDatabase db = getReadableDatabase();
+        String query = "SELECT " + PurchaseHistoryValues.PRICE + " FROM " + PurchaseHistoryValues.TABLE_NAME + " WHERE " + PurchaseHistoryValues.HISTORY_ID + " = " + historyID;
+        Cursor cursor = db.rawQuery(query, null);
+        double totalPrice = 0;
+        while (cursor.moveToNext()){
+            totalPrice += cursor.getDouble(cursor.getColumnIndex(PurchaseHistoryValues.PRICE));
+        }
+        cursor.close();
+        return totalPrice;
+    }
+    public String getDateFromHistoryID(int historyID){
+        SQLiteDatabase db = getReadableDatabase();
+        String query = "SELECT " + PurchaseHistoryValues.DATE + " FROM " + PurchaseHistoryValues.TABLE_NAME + " WHERE " + PurchaseHistoryValues.HISTORY_ID + " = " + historyID;
+        Cursor cursor = db.rawQuery(query, null);
+        String date = cursor.getString(cursor.getColumnIndex(PurchaseHistoryValues.DATE);
+        cursor.close();
+        return date;
     }
 
 
---
+++ projects/project-02/JerseyApp/app/src/main/java/gg/patrickcummins/myapplication/PurchaseHistoryActivity.java
@@ -0,0 +1,13 @@
+package gg.patrickcummins.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PurchaseHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_purchase_history);
+    }
+}
