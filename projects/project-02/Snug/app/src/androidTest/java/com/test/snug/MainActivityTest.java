7234bc4906ec98fc396fa339b3c37f166000bebb
langsmith <langstonlmcs@gmail.com>
2016-07-16 11:04:44 -0700

    Setting up Espresso tests

--- projects/project-02/Snug/app/src/androidTest/java/com/test/snug/MainActivityTest.java
+++ projects/project-02/Snug/app/src/androidTest/java/com/test/snug/MainActivityTest.java
@@ -11,6 +11,15 @@ import org.junit.runner.RunWith;
  * Created by LangstonSmith on 7/15/16.
  */
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.clearText;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
 @RunWith(AndroidJUnit4.class)
 
 public class MainActivityTest {
@@ -19,15 +28,14 @@ public class MainActivityTest {
     public ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
 
 
-
     @Test
     public void testWhetherUIElementsAreVisible() throws Exception {
         // test that answer text view is visible
 
-        onView.
+        //onView doesn't work! Not sure why! I've added all the necessary things to my gradle file, etc.
+
+        onView(withId(R.id.))
+                .check(matches(isDisplayed()));
+
 
     }
-
-
-
-}
--- projects/project-02/Snug/app/src/androidTest/java/com/test/snug/SingleHatViewActivityTest.java
+++ projects/project-02/Snug/app/src/androidTest/java/com/test/snug/SingleHatViewActivityTest.java
@@ -12,6 +12,14 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.clearText;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 public class SingleHatViewActivityTest {
