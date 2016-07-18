9ed6fab5b4f3cdf637b7cf4ea6912329fb027086
Audrey So <audreyeso@Audreys-MacBook-Air.local>
2016-07-18 14:37:49 -0700

    JSON GSON practice

---
+++ week-7-networking/31-monday-JSON/lessons/gson-lesson/GSON_Practice/app/src/main/java/com/test/gson_practice/Bookshelf.java
@@ -0,0 +1,17 @@
+package com.test.gson_practice;
+
+/**
+ * Created by audreyeso on 7/18/16.
+ */
+public class Bookshelf {
+    private Book [] books;
+
+
+    public Book[] getBooks() {
+        return books;
+    }
+
+    public void setBooks(Book[] books) {
+        this.books = books;
+    }
+}
---
+++ week-7-networking/31-monday-JSON/lessons/gson-lesson/GSON_Practice/app/src/main/java/com/test/gson_practice/Cover.java
@@ -0,0 +1,26 @@
+package com.test.gson_practice;
+
+/**
+ * Created by audreyeso on 7/18/16.
+ */
+public class Cover {
+
+    private String color;
+    private String material;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+}
