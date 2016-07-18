4fcb681d7a52c2daaf3171cf099589d0edb56d93
Michael Jeffress <michaeljeffress@Michaels-MacBook-Pro.local>
2016-07-18 16:37:02 -0700

    lab

---
+++ week-7-networking/31-monday-JSON/labs/gson-lab/starter-code/GSON-lab/app/src/main/java/com/example/hollis/gson_example/Weather.java
@@ -0,0 +1,16 @@
+package com.example.hollis.gson_example;
+
+/**
+ * Created by michaeljeffress on 7/18/16.
+ */
+public class Weather {
+    private Query query;
+
+    public Query getQuery() {
+        return query;
+    }
+
+    public void setQuery(Query query) {
+        this.query = query;
+    }
+}
---
+++ week-7-networking/31-monday-JSON/labs/gson-lab/starter-code/GSON-lab/app/src/main/java/com/example/hollis/gson_example/Wind.java
@@ -0,0 +1,34 @@
+package com.example.hollis.gson_example;
+
+/**
+ * Created by michaeljeffress on 7/18/16.
+ */
+public class Wind {
+    private int chill;
+    private int direction;
+    private int speed;
+
+    public int getChill() {
+        return chill;
+    }
+
+    public void setChill(int chill) {
+        this.chill = chill;
+    }
+
+    public int getDirection() {
+        return direction;
+    }
+
+    public void setDirection(int direction) {
+        this.direction = direction;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+}
