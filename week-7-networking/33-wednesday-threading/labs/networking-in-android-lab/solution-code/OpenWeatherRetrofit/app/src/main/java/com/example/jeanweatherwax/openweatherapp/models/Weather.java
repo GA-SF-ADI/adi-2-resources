c6b30b205e12756a652516c91281874feeb76fd8
jeanmw <jean.weatherwax@gmail.com>
2016-07-19 14:20:47 -0700

    updating retrofit lesson/lab

---
+++ week-7-networking/33-wednesday-threading/labs/networking-in-android-lab/solution-code/OpenWeatherRetrofit/app/src/main/java/com/example/jeanweatherwax/openweatherapp/models/Weather.java
@@ -0,0 +1,93 @@
+package com.example.jeanweatherwax.openweatherapp.models;
+
+import com.google.gson.annotations.Expose;
+
+/**
+ * Created by jeanweatherwax on 7/13/16.
+ */
+public class Weather {
+
+    @Expose
+    private Integer id;
+    @Expose
+    private String main;
+    @Expose
+    private String description;
+    @Expose
+    private String icon;
+
+    /**
+     *
+     * @return
+     * The id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     *
+     * @param id
+     * The id
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     *
+     * @return
+     * The main
+     */
+    public String getMain() {
+        return main;
+    }
+
+    /**
+     *
+     * @param main
+     * The main
+     */
+    public void setMain(String main) {
+        this.main = main;
+    }
+
+    /**
+     *
+     * @return
+     * The description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     *
+     * @param description
+     * The description
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     *
+     * @return
+     * The icon
+     */
+    public String getIcon() {
+        return icon;
+    }
+
+    /**
+     *
+     * @param icon
+     * The icon
+     */
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+
+
+}
