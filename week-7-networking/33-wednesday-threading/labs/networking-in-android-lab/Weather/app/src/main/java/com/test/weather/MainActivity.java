844aa441a09d4845ba2c4be04ca07f00382742fa
langsmith <langstonlmcs@gmail.com>
2016-07-20 15:52:21 -0700

    Almost done with weather Retrofit lab

--- week-7-networking/33-wednesday-threading/labs/networking-in-android-lab/Weather/app/src/main/java/com/test/weather/MainActivity.java
+++ week-7-networking/33-wednesday-threading/labs/networking-in-android-lab/Weather/app/src/main/java/com/test/weather/MainActivity.java
@@ -1,13 +1,124 @@
 package com.test.weather;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.test.weather.models.Forecast;
+
+import org.w3c.dom.Text;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+
+            Button fetchWeatherButton = (Button) findViewById(R.id.button);
+
+            fetchWeatherButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    getForecastForCity();
+
+                }
+            });
+
+
+        } else {
+
+            Toast.makeText(MainActivity.this, "Looks like you don't have internet " +
+                    "connectivity!\n Please try turning on your mobile data or connecting" +
+                    " to wifi.", Toast.LENGTH_SHORT).show();
+
+
+        }
+
     }
+
+
+    private void getForecastForCity(String cityName) {
+
+        String baseUrl = "https://api.openweathermap.org";
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        RetroInterface retroInterface = retrofit.create(RetroInterface.class);
+
+        Call<Forecast> call = retroInterface.getForecast(cityName, "002b37154b02170b9f4015bde45b169b");
+
+
+        call.enqueue(new Callback<Forecast>() {
+            @Override
+            public void onResponse(Call<Forecast> call, Response<Forecast> response) {
+
+                try {
+
+                    String finalCityName = response.body().getName();
+
+                    String finalWeather = response.body().getWeather().ge
+
+                    Integer finalPressure = response.body().getMain().getPressure();
+
+                    Integer finalHumidity = response.body().getMain().getHumidity();
+
+                    double finalTemp = response.body().getMain().getTemp();
+
+
+                    TextView finalCityNameView = (TextView) findViewById(R.id.city);
+                    TextView finalCityWeatherView = (TextView) findViewById(R.id.description);
+                    TextView finalCityPressureView = (TextView) findViewById(R.id.pressure);
+                    TextView finalCityHumidityView = (TextView) findViewById(R.id.humidity);
+                    TextView finalCityTempView = (TextView) findViewById(R.id.temperature);
+
+
+
+                    finalCityNameView.setText(finalCityName);
+                    finalCityWeatherView.setText(finalWeather);
+                    finalCityPressureView.setText(finalPressure);
+                    finalCityHumidityView.setText(finalHumidity);
+                    finalCityTempView.setText(finalTemp);
+
+
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+
+            }
+        });
+
+
+    }
+
+
 }
---
+++ week-7-networking/33-wednesday-threading/labs/networking-in-android-lab/Weather/app/src/main/java/com/test/weather/models/Coord.java
@@ -0,0 +1,54 @@
+package com.test.weather.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by LangstonSmith on 7/20/16.
+ */
+public class Coord {
+
+    @SerializedName("lon")
+    @Expose
+    private Double lon;
+    @SerializedName("lat")
+    @Expose
+    private Double lat;
+
+    /**
+     *
+     * @return
+     * The lon
+     */
+    public Double getLon() {
+        return lon;
+    }
+
+    /**
+     *
+     * @param lon
+     * The lon
+     */
+    public void setLon(Double lon) {
+        this.lon = lon;
+    }
+
+    /**
+     *
+     * @return
+     * The lat
+     */
+    public Double getLat() {
+        return lat;
+    }
+
+    /**
+     *
+     * @param lat
+     * The lat
+     */
+    public void setLat(Double lat) {
+        this.lat = lat;
+    }
+
+}
