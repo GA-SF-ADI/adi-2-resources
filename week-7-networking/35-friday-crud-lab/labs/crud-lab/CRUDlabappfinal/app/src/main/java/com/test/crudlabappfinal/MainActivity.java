8f390d3ae2d1d53a91dd5e06f0a64aeef06684d4
langsmith <langstonlmcs@gmail.com>
2016-07-22 11:32:18 -0700

    Almost done with layouts

---
+++ week-7-networking/35-friday-crud-lab/labs/crud-lab/CRUDlabappfinal/app/src/main/java/com/test/crudlabappfinal/MainActivity.java
@@ -0,0 +1,42 @@
+package com.test.crudlabappfinal;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, CreateBookActivity.class);
+                startActivity(intent);
+                finish();
+
+
+            }
+        });
+
+
+
+        //TODO: Set up list view
+
+
+
+
+    }
+
+
+}
