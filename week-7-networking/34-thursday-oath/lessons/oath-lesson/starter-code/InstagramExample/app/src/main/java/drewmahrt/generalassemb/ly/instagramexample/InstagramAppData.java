04f21ec6c1a156fd1691f9714d6999bbbde3d35d
jon k <jonkk.88@gmail.com>
2016-07-21 17:40:41 -0700

    instagram

--- week-7-networking/34-thursday-oath/lessons/oath-lesson/starter-code/InstagramExample/app/src/main/java/drewmahrt/generalassemb/ly/instagramexample/InstagramAppData.java
+++ week-7-networking/34-thursday-oath/lessons/oath-lesson/starter-code/InstagramExample/app/src/main/java/drewmahrt/generalassemb/ly/instagramexample/InstagramAppData.java
@@ -5,9 +5,9 @@ package drewmahrt.generalassemb.ly.instagramexample;
  */
 public class InstagramAppData {
     // TODO Fill in the three lines from your client info on instagrams developer site
-    public static final String CLIENT_ID = "";
-    public static final String CLIENT_SECRET = "";
-    public static final String CALLBACK_URL = "";
+    public static final String CLIENT_ID = "f19837fff7314e18827fa96e2906fe73";
+    public static final String CLIENT_SECRET = "c0b0bff951b6484bbf03a0d29c2603bc";
+    public static final String CALLBACK_URL = "http://localhost:3000";
 
 
     public static final String AUTH_CODE_KEY = "authorization_code";
