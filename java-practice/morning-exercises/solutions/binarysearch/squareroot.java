9ce64dc4bc8472ae64b0d3e529517874f073255a
Jean Weatherwax <jeanmw@users.noreply.github.com>
2016-07-25 09:12:13 -0700

    Create squareroot.java

---
+++ java-practice/morning-exercises/solutions/binarysearch/squareroot.java
@@ -0,0 +1,22 @@
+public class Solution {
+    
+	public int sqrt(int a) {
+	    
+	    long low = 0;
+	    long high = a;
+	    while(low <= high) {
+	        long mid = (high + low)/2;
+	        long square = mid*mid;
+	        if(square == a) {
+	            return (int)mid;
+	        } else if (square < a) {
+	            low = mid + 1 ;
+	        } else {
+	            high = mid - 1;
+	        }
+	    }
+        
+        return (int)high;	    
+
+	}
+}
