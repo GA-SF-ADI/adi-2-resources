76f1b3a764f07610bb4b6e89e7a64124be115064
Finnegan1441 <patrickcummins2009@gmail.com>
2016-07-25 10:53:44 -0700

    Idk what this is but i need to switch branch ¯\_(ツ)_/¯

--- week-7-networking/35-friday-crud-lab/labs/crud-lab/MyApplication/app/src/main/java/gg/patrickcummins/week7fridaybooklab/EditBookActivity.java
+++ week-7-networking/35-friday-crud-lab/labs/crud-lab/MyApplication/app/src/main/java/gg/patrickcummins/week7fridaybooklab/EditBookActivity.java
@@ -76,7 +76,7 @@ public class EditBookActivity extends AppCompatActivity {
                         }
                     });
                     finish();
-                    
+
 
 
                 }
