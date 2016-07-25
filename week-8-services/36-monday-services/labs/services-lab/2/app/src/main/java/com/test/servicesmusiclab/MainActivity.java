6f25a6f65bd9de37316e325c428899881d64baa5
Audrey So <audreyeso@Audreys-MacBook-Air.local>
2016-07-25 16:32:04 -0700

    creates play, pause, stop buttons and set intent for Music Service

--- week-8-services/36-monday-services/labs/services-lab/2/app/src/main/java/com/test/servicesmusiclab/MainActivity.java
+++ week-8-services/36-monday-services/labs/services-lab/2/app/src/main/java/com/test/servicesmusiclab/MainActivity.java
@@ -1,7 +1,10 @@
 package com.test.servicesmusiclab;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,60 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setViews();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Intent intent = new Intent(MainActivity.this, MusicService.class);
+        stopService(intent);
+
+        intent = new Intent(MainActivity.this, MusicService.class);
+        stopService(intent);
+    }
+
+    private void setViews() {
+        setServiceViews();
+    }
+
+    private void setServiceViews() {
+        Button playButton = (Button) findViewById(R.id.play_button);
+        Button stopButton = (Button) findViewById(R.id.stop_button);
+        Button pauseButton = (Button) findViewById(R.id.pause_button);
+
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO START your CustomService here
+
+                Intent intent = new Intent(MainActivity.this, MusicService.class);
+                startService(intent);
+
+            }
+        });
+
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO STOP your CustomService here
+                Intent intent = new Intent(MainActivity.this, MusicService.class);
+                stopService(intent);
+
+            }
+        });
+
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO PAUSE your CustomService here
+                Intent intent = new Intent(MainActivity.this, MusicService.class);
+                stopService(intent);
+
+            }
+        });
     }
 }
+
