2a9ca9bb4f0764694dc5bad15f9f8535d91fa117
jeanmw <jean.weatherwax@gmail.com>
2016-07-26 14:54:35 -0700

    updating readme for external content provider

--- week-8-services/39-thursday-external-content-provider/lessons/external-content-provider/solution-code/ContactsExample/app/src/main/java/drewmahrt/generalassemb/ly/contactsexample/MainActivity.java
+++ week-8-services/39-thursday-external-content-provider/lessons/external-content-provider/solution-code/ContactsExample/app/src/main/java/drewmahrt/generalassemb/ly/contactsexample/MainActivity.java
@@ -12,8 +12,6 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
@@ -43,27 +41,6 @@ public class MainActivity extends AppCompatActivity {
         return getContentResolver().query(uri, projection, null, null, null);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
 
     // Called when the user is performing an action which requires the app to read the
