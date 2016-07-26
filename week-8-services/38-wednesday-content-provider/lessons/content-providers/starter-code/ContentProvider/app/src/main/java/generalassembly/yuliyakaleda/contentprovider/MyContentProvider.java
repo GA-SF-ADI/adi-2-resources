83aec8753ede48c2b444269041c7be54c666bbba
jeanmw <jean.weatherwax@gmail.com>
2016-07-26 11:22:48 -0700

    helpful comments in starter code for content provider lesson

---
+++ week-8-services/38-wednesday-content-provider/lessons/content-providers/starter-code/ContentProvider/app/src/main/java/generalassembly/yuliyakaleda/contentprovider/MyContentProvider.java
@@ -0,0 +1,35 @@
+package generalassembly.yuliyakaleda.contentprovider;
+
+import android.content.ContentProvider;
+
+/**
+ * Created by jeanweatherwax on 7/26/16.
+ */
+
+//Extend ContentProvider
+public class MyContentProvider extends ContentProvider{
+
+
+    /**Create our Content Provider, add static Strings*/
+
+    /**Create URI matcher and add URIs */
+
+
+    /**Get DBHandler in onCreate */
+    @Override
+    public boolean onCreate() {
+        return false;
+    }
+
+    /**Override insert */
+
+    /**Override query */
+
+    /** Override getType */
+
+    /** Override update */
+
+    /** Override delete */
+
+
+}
