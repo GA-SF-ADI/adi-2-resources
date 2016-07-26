f9e36d0b732bd94e40615fc2f2c2740902310960
jeanmw <jean.weatherwax@gmail.com>
2016-07-26 12:49:52 -0700

    fixing solution code for external content provider lesson to be current

--- week-8-services/39-thursday-external-content-provider/lessons/external-content-provider/solution-code/ContactsExample/app/src/main/java/drewmahrt/generalassemb/ly/contactsexample/MainActivity.java
+++ week-8-services/39-thursday-external-content-provider/lessons/external-content-provider/solution-code/ContactsExample/app/src/main/java/drewmahrt/generalassemb/ly/contactsexample/MainActivity.java
@@ -1,14 +1,20 @@
 package drewmahrt.generalassemb.ly.contactsexample;
 
+import android.Manifest;
+import android.annotation.TargetApi;
 import android.content.ContentResolver;
 import android.content.ContentValues;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.provider.UserDictionary;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -18,11 +24,13 @@ import android.view.MenuItem;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     SimpleCursorAdapter mCursorAdapter;
+    private static final int READ_CONTACTS_PERMISSIONS_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,63 +39,15 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        ListView listView = (ListView)findViewById(R.id.contacts_list);
-
-        final String[] projection = new String[] { ContactsContract.Contacts._ID, ContactsContract.Contacts.DISPLAY_NAME };
-        final Uri uri = ContactsContract.Contacts.CONTENT_URI;
-
-        Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
-
-        while(cursor.moveToNext()){
-            Log.d(MainActivity.class.getName(),"ID: "+cursor.getString(0)+" Name: "+cursor.getString(1));
-        }
-
-        mCursorAdapter = new SimpleCursorAdapter(this,android.R.layout.simple_list_item_1,cursor,new String[]{ContactsContract.Contacts.DISPLAY_NAME},new int[]{android.R.id.text1},0);
-        listView.setAdapter(mCursorAdapter);
-
-        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                Log.d(MainActivity.class.getName(),"Clicked on ID: "+id);
-//                getContentResolver().delete(uri,null,null);
-
-                Cursor current = getContentResolver().query(ContactsContract.Contacts.CONTENT_URI,
-                        null, ContactsContract.Contacts._ID + "=" + id, null, null);
-                if(current.moveToFirst()) {
-                    String lookupKey = current.getString(current.getColumnIndex(
-                            ContactsContract.Contacts.LOOKUP_KEY));
-                    Uri lookupUri = Uri.withAppendedPath(ContactsContract.
-                            Contacts.CONTENT_LOOKUP_URI, lookupKey);
-                    System.out.println("The uri is " + lookupUri.toString());
-                    getContentResolver().delete(lookupUri, ContactsContract.Contacts._ID + "=" + id, null);
-
-                    Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
-                    mCursorAdapter.swapCursor(cursor);
-                }else {
-                    Snackbar.make(view, "Error deleting contact", Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show();
-                }
-                return false;
-            }
-        });
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
+        getPermissionToReadUserContacts();
 
     }
 
     private Cursor getContacts() {
         // Run query
         Uri uri = ContactsContract.Contacts.CONTENT_URI;
-        String[] projection = new String[] { ContactsContract.Contacts._ID,
-                ContactsContract.Contacts.DISPLAY_NAME };
+        String[] projection = new String[]{ContactsContract.Contacts._ID,
+                ContactsContract.Contacts.DISPLAY_NAME};
 
 
         return getContentResolver().query(uri, projection, null, null, null);
@@ -114,4 +74,102 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    // Called when the user is performing an action which requires the app to read the
+    // user's contacts
+    @TargetApi(23)
+    public void getPermissionToReadUserContacts() {
+        // 1) Use the support library version ContextCompat.checkSelfPermission(...) to avoid
+        // checking the build version since Context.checkSelfPermission(...) is only available
+        // in Marshmallow
+        // 2) Always check for permission (even if permission has already been granted)
+        // since the user can revoke permissions at any time through Settings
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // The permission is NOT already granted.
+            // Check if the user has been asked about this permission already and denied
+            // it. If so, we want to give more explanation about why the permission is needed.
+            if (shouldShowRequestPermissionRationale(
+                    Manifest.permission.READ_CONTACTS)) {
+                // Show our own UI to explain to the user why we need to read the contacts
+                // before actually requesting the permission and showing the default UI
+            }
+
+            // Fire off an async request to actually get the permission
+            // This will show the standard permission request dialog UI
+            requestPermissions(new String[]{Manifest.permission.READ_CONTACTS},
+                    READ_CONTACTS_PERMISSIONS_REQUEST);
+        }
+    }
+
+    // Callback with the request from calling requestPermissions(...)
+    @TargetApi(23)
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        // Make sure it's our original READ_CONTACTS request
+        if (requestCode == READ_CONTACTS_PERMISSIONS_REQUEST) {
+            if (grantResults.length == 1 &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Read Contacts permission granted", Toast.LENGTH_SHORT).show();
+                ListView listView = (ListView) findViewById(R.id.contacts_list);
+
+                final String[] projection = new String[]{ContactsContract.Contacts._ID, ContactsContract.Contacts.DISPLAY_NAME};
+                final Uri uri = ContactsContract.Contacts.CONTENT_URI;
+
+                Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
+
+                while (cursor.moveToNext()) {
+                    Log.d(MainActivity.class.getName(), "ID: " + cursor.getString(0) + " Name: " + cursor.getString(1));
+                }
+
+                mCursorAdapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1, cursor, new String[]{ContactsContract.Contacts.DISPLAY_NAME}, new int[]{android.R.id.text1}, 0);
+                listView.setAdapter(mCursorAdapter);
+
+                listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+                    @Override
+                    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                        Log.d(MainActivity.class.getName(), "Clicked on ID: " + id);
+//                getContentResolver().delete(uri,null,null);
+
+                        Cursor current = getContentResolver().query(ContactsContract.Contacts.CONTENT_URI,
+                                null, ContactsContract.Contacts._ID + "=" + id, null, null);
+                        if (current.moveToFirst()) {
+                            String lookupKey = current.getString(current.getColumnIndex(
+                                    ContactsContract.Contacts.LOOKUP_KEY));
+                            Uri lookupUri = Uri.withAppendedPath(ContactsContract.
+                                    Contacts.CONTENT_LOOKUP_URI, lookupKey);
+                            System.out.println("The uri is " + lookupUri.toString());
+                            getContentResolver().delete(lookupUri, ContactsContract.Contacts._ID + "=" + id, null);
+
+                            Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
+                            mCursorAdapter.swapCursor(cursor);
+                        } else {
+                            Snackbar.make(view, "Error deleting contact", Snackbar.LENGTH_LONG)
+                                    .setAction("Action", null).show();
+                        }
+                        return false;
+                    }
+                });
+
+
+            } else {
+                // showRationale = false if user clicks Never Ask Again, otherwise true
+                boolean showRationale = shouldShowRequestPermissionRationale(Manifest.permission.READ_CONTACTS);
+
+                if (showRationale) {
+                    // do something here to handle degraded mode
+                } else {
+                    Toast.makeText(this, "Read Contacts permission denied", Toast.LENGTH_SHORT).show();
+                }
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
 }
+
+
