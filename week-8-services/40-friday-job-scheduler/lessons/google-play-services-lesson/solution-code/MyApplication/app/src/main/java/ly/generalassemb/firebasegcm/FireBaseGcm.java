c2820893527d54e4dd442b9bf1dd560bd2beb9ef
Aleksandr Tomak <alex@Aleksandrs-MacBook-Pro.local>
2016-07-28 16:19:33 -0700

    Create notification of the push notification

--- week-8-services/40-friday-job-scheduler/lessons/google-play-services-lesson/solution-code/MyApplication/app/src/main/java/ly/generalassemb/firebasegcm/FireBaseGcm.java
+++ week-8-services/40-friday-job-scheduler/lessons/google-play-services-lesson/solution-code/MyApplication/app/src/main/java/ly/generalassemb/firebasegcm/FireBaseGcm.java
@@ -1,5 +1,8 @@
 package ly.generalassemb.firebasegcm;
 
+import android.app.NotificationManager;
+import android.content.Context;
+import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
@@ -9,6 +12,7 @@ import com.google.firebase.messaging.RemoteMessage;
  * Created by alex on 7/28/16.
  */
 public class FireBaseGcm extends FirebaseMessagingService {
+    private static final int NOTIF_ID = 9999;
 
     private static final String TAG = "FireBaseGcm";
     @Override
@@ -18,5 +22,14 @@ public class FireBaseGcm extends FirebaseMessagingService {
         Log.i(TAG, "onMessageReceived: " + remoteMessage.getFrom());;
         Log.i(TAG, "onMessageReceived: title " + remoteMessage.getNotification().getTitle());
         Log.i(TAG, "onMessageReceived: body " + remoteMessage.getNotification().getBody());
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext());
+
+        builder.setContentTitle("Message from the cloud");
+        builder.setContentText(remoteMessage.getNotification().getBody());
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(NOTIF_ID, builder.build());
     }
 }
