fee97403a8296f6ba36e0d3aa7500389a325b6ee
Jean Weatherwax <jeanmw@users.noreply.github.com>
2016-07-29 09:48:07 -0700

    Update squareroot.java

--- java-practice/morning-exercises/solutions/binarysearch/squareroot.java
+++ java-practice/morning-exercises/solutions/binarysearch/squareroot.java
@@ -17,4 +17,33 @@
         return (int)high;	    
 
 	}
+	
+	
+	///alternate
+	
+	public static int findSquareRoot(int left, int right, int inputNumber){
+
+        // base condition
+        if (inputNumber ==0 || inputNumber == 1){
+            return inputNumber;
+        }
+
+        int mid = (left + right)/2;
+
+        // if square of mid value is less or equal to input value and 
+        // square of mid+1 is less than input value. We found the answer. 
+        if (mid*mid <= inputNumber && (mid+1)*(mid+1) > inputNumber){
+            return mid;
+        }
+
+        // if input number is greater than square of mid, we need 
+        // to find in right hand side of mid else in left hand side.
+        if (mid*mid < inputNumber){
+            return findSquareRoot(mid+1, right, inputNumber);
+        }
+        else{
+            return findSquareRoot(left, mid-1, inputNumber);
+        }
+
+    }
 
