cf5aeee4b3cec04bedecfb2d5abb8e6de7630a3d
Audrey So <audreyeso@Audreys-MacBook-Air.local>
2016-08-06 14:08:10 -0700

    adds JSON models from Open Library API

---
+++ projects/BookScanner2/app/src/main/java/com/test/bookscanner2/models/Link.java
@@ -0,0 +1,55 @@
+package com.test.bookscanner2.models;
+
+/**
+ * Created by audreyeso on 8/6/16.
+ */
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+
+public class Link {
+
+    @SerializedName("url")
+    @Expose
+    private String url;
+    @SerializedName("title")
+    @Expose
+    private String title;
+
+    /**
+     *
+     * @return
+     * The url
+     */
+    public String getUrl() {
+        return url;
+    }
+
+    /**
+     *
+     * @param url
+     * The url
+     */
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    /**
+     *
+     * @return
+     * The title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     *
+     * @param title
+     * The title
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+}
