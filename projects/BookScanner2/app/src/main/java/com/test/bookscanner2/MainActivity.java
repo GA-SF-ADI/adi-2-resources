ee6a43a9d6d165cf9cf21a01f7ad973efa487464
Audrey So <audreyeso@Audreys-MacBook-Air.local>
2016-08-06 18:41:11 -0700

    add logo and icon launcher

--- projects/BookScanner2/app/src/main/java/com/test/bookscanner2/MainActivity.java
+++ projects/BookScanner2/app/src/main/java/com/test/bookscanner2/MainActivity.java
@@ -1,15 +1,28 @@
 package com.test.bookscanner2;
 
 import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.test.bookscanner2.models.Example;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +33,9 @@ public class MainActivity extends AppCompatActivity {
     static final String QR_MODE = "QR_CODE_MODE";
 
     private Button bookListButton;
+    private TextView textView;
+    private String baseUrl = "https://openlibrary.org/api/";
+    private String contents;
 
 
     @Override
@@ -27,6 +43,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getBookDescription();
+
+
+
+        textView = (TextView) findViewById(R.id.textview);
         bookListButton = (Button) findViewById(R.id.book_list_button);
         bookListButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(MainActivity.this, BookListActvity.class);
                 startActivity(intent);
             }
+
         });
     }
 
@@ -42,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(ACTION_SCAN);
             intent.putExtra(SCAN_MODE, PRODUCT_MODE);
             startActivityForResult(intent, 0);
+            //getBookDescription(); // getting book description here
         } catch (ActivityNotFoundException e) {
             showDialog(MainActivity.this, "No Scanner Found",
                     "Download a scanner code activity?", "Yes", "No").show();
@@ -88,7 +111,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
         if (requestCode == 0) {
             if (resultCode == RESULT_OK) {
-                String contents = intent.getStringExtra("SCAN_RESULT");
+                contents = intent.getStringExtra("SCAN_RESULT");
                 String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
                 Toast.makeText(this, "Content:" + contents + "Format:" +
                         format, Toast.LENGTH_LONG).show();
@@ -96,4 +119,52 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
+
+    protected void getBookDescription() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl(baseUrl)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+
+            OpenLibraryAPI openLibraryAPI = retrofit.create(OpenLibraryAPI.class);
+            //replace ISBN with contents //"ISBN:0439639034"- flyguy
+            //https://openlibrary.org/api/books?bibkeys=ISBN:0439639034&jscmd=data&format=json
+            Call<Example> call = openLibraryAPI.getBookDescription(contents,"data","json");
+
+            call.enqueue(new Callback<Example>() {
+                @Override
+                public void onResponse(Call<Example> call, Response<Example> response) {
+                    try {
+
+                        Log.d("on response", "onResponse: " + response.body().getISBN9780980200447().getTitle().toString());
+
+
+                        String bookTitle = response.body().getISBN9780980200447().getTitle().toString();
+                        String bookAuthor = response.body().getISBN9780980200447().getAuthors().toString();
+
+                        textView.setText(bookTitle + bookAuthor);
+
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<Example> call, Throwable t) {
+                    t.printStackTrace();
+                }
+            });
+        } else {
+            Toast.makeText(MainActivity.this, "No WIFI", Toast.LENGTH_LONG).show();
+
+        }
+
+    }
+
 }
