4823631cb20412ec6f4711cf98a23e36acbd9c57
jon k <jonkk.88@gmail.com>
2016-08-09 13:54:00 -0700

    inserstion sort added

--- week-10-cs-concepts/47-tuesday-sorts/lessons/sorting-algorithms-lesson/starter-code/InsertionSort.java
+++ week-10-cs-concepts/47-tuesday-sorts/lessons/sorting-algorithms-lesson/starter-code/InsertionSort.java
@@ -14,6 +14,18 @@ public class InsertionSort {
 
 	private static int[] doInsertionSort(int[] input) {
 		//Your code here
+		int temp;
+		for (int i = 1; i <input.length ; i++) {
+			for (int j = i; j > 0; j--) {
+				if(input[j]<input[j-1]){
+					temp = input[j];
+					input[j] = input[j-1];
+					input[j-1] = temp;
+					System.out.println("Swapped " + j + " with " + temp);
+				}
+			}
+
+		}
 		
         return input;
 	}
